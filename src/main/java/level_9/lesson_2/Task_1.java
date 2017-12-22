2b4f67bcd8bdfe99840a01012dbea5bf280761be
ivan <korol.vanja2008@gmail.com>
2017-12-22 14:27:48 +0200

    compleat level_9.Lesson_2.Task_1,2

---
+++ src/main/java/level_9/lesson_2/Task_1.java
@@ -0,0 +1,43 @@
+package level_9.lesson_2;
+
+/**
+ * @author by Ivan Korol on 11/8/2017.
+ */
+public class Task_1 {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    public static StackTraceElement[] method1() {
+        method2();
+        return Thread.currentThread().getStackTrace();
+        //напишите тут ваш код
+    }
+
+    public static StackTraceElement[] method2() {
+        method3();
+        return Thread.currentThread().getStackTrace();
+        //напишите тут ваш код
+    }
+
+    public static StackTraceElement[] method3() {
+        method4();
+        return Thread.currentThread().getStackTrace();
+        //напишите тут ваш код
+    }
+
+    public static StackTraceElement[] method4() {
+        method5();
+        return Thread.currentThread().getStackTrace();
+        //напишите тут ваш код
+    }
+
+    public static StackTraceElement[] method5() {
+//        StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+//        for (StackTraceElement el:
+//             elements) {
+//            System.out.println(el);
+//        }
+        return Thread.currentThread().getStackTrace();//напишите тут ваш код
+    }
+}
