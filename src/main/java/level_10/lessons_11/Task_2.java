0818b2e07652c28cdd366382ca60e8bf4bf0b4a9
Ivan Korol <korol.vanja2008@gmail.com>
2017-12-31 12:10:59 +0200

    compleated level_10.lesson_11.Task_2

---
+++ src/main/java/level_10/lessons_11/Task_2.java
@@ -0,0 +1,12 @@
+package level_10.lessons_11;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int c = 3;
+        int e =  2;
+        double d = (double) a + b / c / (float) e;
+        System.out.println(d);
+    }
+}
