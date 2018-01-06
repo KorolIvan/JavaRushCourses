6a240df9fbecc3cb679abd7f7716b46941c63237
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-06 12:43:25 +0200

    compleated level_11.lesson_6.Task_1

---
+++ src/main/java/level_11/lessons_6/Task_1.java
@@ -0,0 +1,14 @@
+package level_11.lessons_6;
+
+public class Task_1 {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegas extends Horse{
+
+    }
+}
