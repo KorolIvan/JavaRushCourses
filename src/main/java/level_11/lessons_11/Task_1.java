64ced2c83489ba5acaa107ef3101d61769c22c69
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-07 09:52:40 +0200

    compleated level_11.lesson_11.Task_1

---
+++ src/main/java/level_11/lessons_11/Task_1.java
@@ -0,0 +1,16 @@
+package level_11.lessons_11;
+
+public class Task_1 {
+        public static void main(String[] args) {
+        }
+
+        //Адам
+        public class Adam {
+
+        }
+
+        //Ева
+        public class Eve extends Adam{
+
+        }
+}
