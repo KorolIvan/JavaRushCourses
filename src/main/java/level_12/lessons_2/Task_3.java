fd66609e171116e86ca4c47d26b4c05087984988
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-07 10:55:35 +0200

    compleated level_12.lesson_2.Task_3

---
+++ src/main/java/level_12/lessons_2/Task_3.java
@@ -0,0 +1,31 @@
+package level_12.lessons_2;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        Pet pet1 = new Cat();
+        Pet cat = pet1.getChild();
+
+        Pet pet2 = new Dog();
+        Pet dog = pet2.getChild();
+    }
+
+    public static class Pet {
+        public Pet getChild() {
+            return new Pet();
+        }
+    }
+
+    public static class Cat extends Pet {
+        @Override
+        public Pet getChild() {
+            return new Cat();
+        }
+    }
+
+    public static class Dog extends Pet {
+        @Override
+        public Pet getChild() {
+            return new Dog();
+        }
+    }
+}
