a2aca2ed002851c1a2bed4a5fcbfa105c91b00b8
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-07 10:49:50 +0200

    compleated level_12.lesson_2.Task1

---
+++ src/main/java/level_11/lessons_11/Task_10.java
@@ -0,0 +1,30 @@
+package level_11.lessons_11;
+
+public class Task_10 {
+    public static void main(String[] args) {
+    }
+
+    public class House extends Assets{
+
+    }
+
+    public class Cat extends Animal{
+
+    }
+
+    public class Car extends Assets{
+
+    }
+
+    public class Dog extends Animal{
+
+    }
+
+    public class Animal {
+
+    }
+
+    public class Assets {
+
+    }
+}
