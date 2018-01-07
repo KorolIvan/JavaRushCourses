3e6f8a8b117a4ec95d81f302ea21a814294003df
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-07 23:15:56 +0200

    compleated level_12.lesson_6.Task_5

---
+++ src/main/java/level_12/lesson_6/Task_5.java
@@ -0,0 +1,39 @@
+package level_12.lesson_6;
+
+public class Task_5 {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public abstract String getName();
+
+        public abstract Pet getChild();
+    }
+
+    public static class Cat extends Pet{
+
+        @Override
+        public String getName() {
+            return null;
+        }
+
+        @Override
+        public Pet getChild() {
+            return null;
+        }
+    }
+
+    public static class Dog extends Pet{
+
+        @Override
+        public String getName() {
+            return null;
+        }
+
+        @Override
+        public Pet getChild() {
+            return null;
+        }
+    }
+}
