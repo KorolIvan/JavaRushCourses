9048e40fbeffdf525962aff9f3b6867b7847f9e4
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-07 10:09:12 +0200

    compleated level_11.lesson_11.Task_9

---
+++ src/main/java/level_11/lessons_11/Task_9.java
@@ -0,0 +1,22 @@
+package level_11.lessons_11;
+
+public class Task_9 {
+    public static void main(String[] args) {
+    }
+
+    public class House {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog {
+
+    }
+}
