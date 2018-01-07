67151b32991f4586499a714331a7b5caea6fd901
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-07 10:05:37 +0200

    compleated level_11.lesson_11.Task_7

---
+++ src/main/java/level_11/lessons_11/Task_7.java
@@ -0,0 +1,26 @@
+package level_11.lessons_11;
+
+public class Task_7 {
+    public static void main(String[] args) {
+    }
+
+    public class Carnivore extends Animal{
+
+    }
+
+    public class Cow extends Animal{
+
+    }
+
+    public class Dog extends Carnivore{
+
+    }
+
+    public class Pig extends Animal{
+
+    }
+
+    public class Animal {
+
+    }
+}
