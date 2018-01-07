6db620fdb8c8088e3cc25d3d3bc9bebcf9287925
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-07 10:53:12 +0200

    compleated level_12.lesson_2.Task_2

---
+++ src/main/java/level_12/lessons_2/Task_2.java
@@ -0,0 +1,22 @@
+package level_12.lessons_2;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        Cow cow = new Whale();
+
+        System.out.println(cow.getName());
+    }
+
+    public static class Cow {
+        public String getName() {
+            return "Я - корова";
+        }
+    }
+
+    public static class Whale extends Cow {
+        @Override
+        public String getName(){
+            return "";
+        }
+    }
+}
