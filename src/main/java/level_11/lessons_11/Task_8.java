ef4ec2e1ddb86736615bbad75a17e1c9b17b370c
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-07 10:06:46 +0200

    compleated level_11.lesson_11.Task_8

---
+++ src/main/java/level_11/lessons_11/Task_8.java
@@ -0,0 +1,22 @@
+package level_11.lessons_11;
+
+public class Task_8 {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet{
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog extends Pet{
+
+    }
+}
