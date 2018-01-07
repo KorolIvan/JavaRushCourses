8bb5b196c5851465c4e2c48adbe7bc6b2caf9fae
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-07 09:57:13 +0200

    compleated level_11.lesson_11.Task_3

---
+++ src/main/java/level_11/lessons_11/Task_3.java
@@ -0,0 +1,23 @@
+package level_11.lessons_11;
+
+public class Task_3 {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish{
+
+    }
+
+    public class Ape extends Animal{
+
+    }
+
+    public class Human extends Ape{
+
+    }
+
+}
