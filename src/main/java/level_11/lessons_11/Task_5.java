2380c08af3a5dce680d22618b47bd0f0067a5e16
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-07 10:01:21 +0200

    compleated level_11.lesson_11.Task_5

---
+++ src/main/java/level_11/lessons_11/Task_5.java
@@ -0,0 +1,23 @@
+package level_11.lessons_11;
+
+public class Task_5 {
+    public static void main(String[] args) {
+    }
+
+    public class Schoolboy {
+
+    }
+
+    public class Student extends Schoolboy{
+
+    }
+
+    public class Worker extends Student{
+
+    }
+
+    public class Slave extends Worker{
+
+    }
+
+}
