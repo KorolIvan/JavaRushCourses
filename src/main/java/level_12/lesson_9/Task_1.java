bb59331fd2c4eee961fe34b0a882beaaf7010e47
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-08 11:14:49 +0200

    compleated level_12.lesson_9.Task_1

---
+++ src/main/java/level_12/lesson_9/Task_1.java
@@ -0,0 +1,12 @@
+package level_12.lesson_9;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Fly {
+        void fly();
+        void speed();
+    }
+}
