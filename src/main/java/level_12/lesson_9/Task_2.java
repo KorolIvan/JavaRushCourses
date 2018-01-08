d68d6b7ce6d33a5799fe541fd44bda7efa26b40f
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-08 11:18:14 +0200

    compleated level_12.lesson_9.Task_2

---
+++ src/main/java/level_12/lesson_9/Task_2.java
@@ -0,0 +1,17 @@
+package level_12.lesson_9;
+
+public class Task_2 {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Fly{
+        boolean fly();
+    }
+    public interface Run{
+        boolean run();
+    }
+    public interface Swim{
+        int deep();
+    }
+}
