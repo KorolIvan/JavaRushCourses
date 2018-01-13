12dbf87bf7f9a795973309c97230ca3d65939797
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-13 16:25:54 +0200

    compleated level_13.lesson_4.Task_3

--- src/main/java/level_13/lesson_4/Task_2.java
+++ src/main/java/level_13/lesson_4/Task_2.java
@@ -1,6 +1,6 @@
 package level_13.lesson_4;
 
-public class TASK_2 {
+public class Task_2 {
     public static void main(String[] args) throws Exception {
         Translator translator = new Translator();
         System.out.println(translator.translate());
