987ef397372194bb4fd53e89f4324bc0d82c25b1
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-13 15:23:18 +0200

    compleated level_13.lesson_2.Task_8

---
+++ src/main/java/level_13/lesson_2/Task_8.java
@@ -0,0 +1,13 @@
+package level_13.lesson_2;
+
+public class Task_8 {
+    public static void main(String[] args) {
+
+    }
+    interface Person {
+        boolean isAlive();
+    }
+    interface Presentable extends Person{
+
+    }
+}
