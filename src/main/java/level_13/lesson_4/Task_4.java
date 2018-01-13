46485e6ce59ce63e4025cc9969f2794b47deeb9b
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-13 16:29:45 +0200

    compleated level_13.lesson_4.Task_4

---
+++ src/main/java/level_13/lesson_4/Task_4.java
@@ -0,0 +1,33 @@
+package level_13.lesson_4;
+
+import java.awt.*;
+
+public class Task_4 {
+    public static void main(String[] args) throws Exception {
+        Fox bigFox = new BigFox();
+        System.out.println(bigFox.getName());
+        System.out.println(bigFox.getColor());
+
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public static abstract class Fox implements Animal {
+        public String getName() {
+            return "Fox";
+        }
+    }
+
+    public static class BigFox extends Fox{
+//        public Color() {
+//            return Color.GRAY;
+//        }
+
+        @Override
+        public Color getColor() {
+            return Color.GRAY;
+        }
+    }
+}
