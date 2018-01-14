b02a3373496805cd57b7e4b3be74aa2b9db00ca3
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-14 09:02:26 +0200

    compleated level_13.lesson_6.Task_1

---
+++ src/main/java/level_13/lesson_6/Task_1.java
@@ -0,0 +1,62 @@
+package level_13.lesson_6;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+    }
+    //может двигаться
+    public interface Movable {
+        void move();
+    }
+
+    //может быть съеден
+    public interface Eatable {
+        void eaten();
+    }
+
+    //может кого-нибудь съесть
+    public interface Eat {
+        void eat();
+    }
+
+    public class Cat implements Movable, Eatable, Eat{
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eaten() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
+    }
+
+    public class Dog implements Movable, Eat{
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
+    }
+
+    public class Mouse implements Movable, Eatable{
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eaten() {
+
+        }
+    }
+}
