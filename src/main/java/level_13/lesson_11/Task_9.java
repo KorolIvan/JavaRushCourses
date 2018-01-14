d57da99f0e7937a1fde8599c080eb3fbd9b7dc51
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-14 18:50:59 +0200

    compleated level_13.lesson_11.Task_9

---
+++ src/main/java/level_13/lesson_11/Task_9.java
@@ -0,0 +1,20 @@
+package level_13.lesson_11;
+
+import java.awt.*;
+
+public class Task_9 {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+
+        Integer getAge();
+    }
+
+    public static abstract class Fox implements Animal{
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
