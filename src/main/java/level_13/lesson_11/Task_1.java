85f5a62cc099986d1722f02ebe9495bd60e788c6
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-14 12:17:27 +0200

    compleated level_13.lesson_11.Task_1

---
+++ src/main/java/level_13/lesson_11/Task_1.java
@@ -0,0 +1,22 @@
+package level_13.lesson_11;
+
+public class Task_1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(SimpleObject.NAME);
+        System.out.println(Button.NAME);
+    }
+
+    interface SimpleObject {
+        String NAME = "SimpleObject";
+
+        void onPress();
+    }
+
+    interface Button extends SimpleObject {
+
+        String NAME = "Submit";
+
+        String onPress(Object o);
+
+    }
+}
