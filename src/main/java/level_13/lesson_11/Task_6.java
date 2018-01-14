b423c0f52230f339633b481e5843bc42b3fecf07
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-14 18:37:56 +0200

    compleated level_13.lesson_11.Task_6

---
+++ src/main/java/level_13/lesson_11/Task_6.java
@@ -0,0 +1,25 @@
+package level_13.lesson_11;
+
+public class Task_6 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Dream.HOBBIE.toString());
+        System.out.println(new Hobbie().INDEX);
+    }
+
+    interface Desire {
+    }
+
+    interface Dream {
+        public static Hobbie HOBBIE = new Hobbie();
+    }
+
+    static class Hobbie implements Desire, Dream {
+        static int INDEX = 1;
+
+        @Override
+        public String toString() {
+            INDEX++;
+            return "" + INDEX;
+        }
+    }
+}
