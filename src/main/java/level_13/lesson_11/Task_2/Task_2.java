cc34dc675f1d58659e054a284d3ddf59979a428b
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-14 12:25:29 +0200

    compleated level_13.lesson_11.Task_2

---
+++ src/main/java/level_13/lesson_11/Task_2/Task_2.java
@@ -0,0 +1,27 @@
+package level_13.lesson_11.Task_2;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        System.out.println(new Today(WeatherType.CLOUDY));
+        System.out.println(new Today(WeatherType.FOGGY));
+        System.out.println(new Today(WeatherType.FROZEN));
+    }
+
+    static class Today implements Weather{
+        private String type;
+
+        Today(String type) {
+            this.type = type;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s for today", this.getWeatherType());
+        }
+
+        @Override
+        public String getWeatherType() {
+            return type;
+        }
+    }
+}
---
+++ src/main/java/level_13/lesson_11/Task_2/Weather.java
@@ -0,0 +1,5 @@
+package level_13.lesson_11.Task_2;
+
+public interface Weather {
+    String getWeatherType();
+}
