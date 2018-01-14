6f3b213c52a8b219e2774449f5c573c88e9ec761
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-14 18:48:46 +0200

    compleated level_13.lesson_11.Task_8

---
+++ src/main/java/level_13/lesson_11/Task_8.java
@@ -0,0 +1,27 @@
+package level_13.lesson_11;
+
+public class Task_8 {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable extends Selectable {
+        void refresh();
+    }
+
+    class Screen implements Updatable{
+
+        @Override
+        public void onSelect() {
+
+        }
+
+        @Override
+        public void refresh() {
+
+        }
+    }
+}
