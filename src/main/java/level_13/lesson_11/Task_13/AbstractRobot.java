319adf9fa64cf951e0db3163f5f2807d00ee0423
Ivan Korol <korol.vanja2008@gmail.com>
2018-01-16 22:04:03 +0200

    compleated level_13.lesson_11.Task_13

--- src/main/java/level_13/lesson_11/Task_13/AbstractRobot.java
+++ src/main/java/level_13/lesson_11/Task_13/AbstractRobot.java
@@ -4,18 +4,18 @@ package level_13.lesson_11.Task_13;
  * @author by Ivan Korol on 11/8/2017.
  */
 public abstract class AbstractRobot implements Attackable, Defensable{
-    private static int hitCount;
+    private static int hitCount = (int) Math.random()*4;
 
     public BodyPart attack() {
         BodyPart attackedBodyPart = null;
-        hitCount = (int) Math.random()*4 + 1;
+        hitCount +=  1;
 
         if (hitCount == 1) {
             attackedBodyPart = BodyPart.ARM;
         } else if (hitCount == 2) {
             attackedBodyPart = BodyPart.HEAD;
         } else if (hitCount == 3) {
-            hitCount = 0;
+            //hitCount = 0;
             attackedBodyPart = BodyPart.LEG;
         } else {
             attackedBodyPart = BodyPart.CHEST;
@@ -25,14 +25,14 @@ public abstract class AbstractRobot implements Attackable, Defensable{
 
     public BodyPart defense() {
         BodyPart defencedBodyPart = null;
-        hitCount = (int) Math.random()*4 + 2;
+        //hitCount += 2;
 
         if (hitCount == 1) {
             defencedBodyPart = BodyPart.HEAD;
         } else if (hitCount == 2) {
             defencedBodyPart = BodyPart.LEG;
         } else if (hitCount == 3) {
-            hitCount = 0;
+            //hitCount = 0;
             defencedBodyPart = BodyPart.ARM;
         }else {
 
