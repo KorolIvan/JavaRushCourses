30ac3af1450c721ac3866d2bf5fc1626e0f3395a
ivan <korol.vanja2008@gmail.com>
2018-01-18 17:08:12 +0200

    compleated level_14.Lesson_8.Task_2

---
+++ src/main/java/level_14/lesson_8/Task_2/BubblyWine.java
@@ -0,0 +1,10 @@
+package level_14.lesson_8.Task_2;
+
+/**
+ * @author by Ivan Korol on 11/8/2017.
+ */
+public class BubblyWine extends Wine{
+    public String getHolidayName(){
+        return "Новый Год";
+    }
+}
---
+++ src/main/java/level_14/lesson_8/Task_2/Wine.java
@@ -0,0 +1,10 @@
+package level_14.lesson_8.Task_2;
+
+/**
+ * @author by Ivan Korol on 11/8/2017.
+ */
+public class Wine extends Drink{
+    public String getHolidayName(){
+        return "День Рождения";
+    }
+}
