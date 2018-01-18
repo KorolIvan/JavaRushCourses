303ddfa99cb2ccd50a8df49c7d6cbbb824137694
ivan <korol.vanja2008@gmail.com>
2018-01-18 13:22:07 +0200

    compleated level_14.Lesson_8.Task_1

---
+++ src/main/java/level_14/lesson_8/Task_1/Solution.java
@@ -0,0 +1,16 @@
+package level_14.lesson_8.Task_1;
+
+
+/**
+ * @author by Ivan Korol on 11/8/2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        println(new WaterBridge());
+        println(new SuspensionBridge());
+    }
+
+    public static void println(Bridge bridge) {
+        System.out.println(bridge.getCarsCount());
+    }
+}
---
+++ src/main/java/level_14/lesson_8/Task_1/SuspensionBridge.java
@@ -0,0 +1,11 @@
+package level_14.lesson_8.Task_1;
+
+/**
+ * @author by Ivan Korol on 11/8/2017.
+ */
+public class SuspensionBridge implements Bridge{
+    @Override
+    public int getCarsCount() {
+        return 100;
+    }
+}
