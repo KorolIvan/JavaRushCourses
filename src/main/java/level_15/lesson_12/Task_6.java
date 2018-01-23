b73ac0d493e0eb02a88d3e5713c2e20cd85e22ca
ivan <korol.vanja2008@gmail.com>
2018-01-23 17:22:23 +0200

    compleated level_15.Lesson_12.Task_6

---
+++ src/main/java/level_15/lesson_12/Task_6.java
@@ -0,0 +1,45 @@
+package level_15.lesson_12;
+
+/**
+ * @author by Ivan Korol on 11/8/2017.
+ */
+public class Task_6 {
+    static {
+        init();
+    }
+
+    static {
+        System.out.println("Static block");
+    }
+
+    {
+        System.out.println("non-static block");
+        printAllFields(this);
+    }
+
+    public int i = 6;
+
+    public String name = "First name";
+
+
+    public Task_6() {
+        System.out.println("Solution constructor");
+        printAllFields(this);
+    }
+
+    public static void init() {
+        System.out.println("static void init()");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("public static void main");
+        Task_6 s = new Task_6();
+    }
+
+    public static void printAllFields(Task_6 obj) {
+        System.out.println("static void printAllFields");
+        System.out.println(obj.i);
+        System.out.println(obj.name);
+
+    }
+}
