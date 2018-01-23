50acdc561ba5f5235c7dd9eab065c45ffe882d9f
ivan <korol.vanja2008@gmail.com>
2018-01-23 17:12:41 +0200

    compleated level_15.Lesson_12.Task_5

---
+++ src/main/java/level_15/lesson_12/Task_5/Solution.java
@@ -0,0 +1,26 @@
+package level_15.lesson_12.Task_5;
+
+/**
+ * @author by Ivan Korol on 11/8/2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    Solution() {
+
+    }
+
+    protected Solution(Integer i) {
+
+    }
+
+    private Solution(int i) {
+
+    }
+
+    public Solution(String s) {
+
+    }
+}
