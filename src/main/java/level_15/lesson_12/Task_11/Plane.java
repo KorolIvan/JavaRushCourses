5496e774c0bae6ea5c494c0012779b891d30226e
ivan <korol.vanja2008@gmail.com>
2018-01-31 15:36:36 +0200

    compleated level_15.Lesson_12.Task_11

---
+++ src/main/java/level_15/lesson_12/Task_11/Plane.java
@@ -0,0 +1,14 @@
+package level_15.lesson_12.Task_11;
+
+/**
+ * @author by Ivan Korol on 11/8/2017.
+ */
+public class Plane implements Flyable {
+    @Override
+    public void fly() {
+
+    }
+
+    public Plane(int i) {
+    }
+}
