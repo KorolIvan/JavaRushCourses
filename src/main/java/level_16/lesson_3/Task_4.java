1d49209311919900879ac96dcf016155bc864185
Ivan Korol <korol.vanja2008@gmail.com>
2018-02-03 10:07:52 +0200

    compleated level_16.lesson_3.Task_4

---
+++ src/main/java/level_16/lesson_3/Task_4.java
@@ -0,0 +1,26 @@
+package level_16.lesson_3;
+
+public class Task_4 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new SpecialThread());
+        thread.start();
+
+        System.out.println("*****************");
+
+        for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+            System.out.println(element);
+        }
+    }
+
+    public static class SpecialThread implements Runnable {
+
+        @Override
+        public void run() {
+            for (StackTraceElement e :
+                    Thread.currentThread().getStackTrace()) {
+                System.out.println(e);
+            }
+
+        }
+    }
+}
