98581f3e715f29c3bf9b5f3cb5221dabf19f80a3
ivan <korol.vanja2008@gmail.com>
2018-02-19 10:20:09 +0200

    compleated level_16.Lesson_13.Task_10

---
+++ src/main/java/level_16/lesson_13/Task_10.java
@@ -0,0 +1,83 @@
+package level_16.lesson_13;
+
+import java.io.*;
+
+/**
+ * @author by Ivan Korol on 11/8/2017.
+ */
+public class Task_10 {
+    public static String firstFileName;
+    public static String secondFileName;
+
+    //add your code here - добавьте код тут
+    static {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            firstFileName = reader.readLine();
+            secondFileName = reader.readLine();
+        }catch (IOException e){
+
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        systemOutPrintln(firstFileName);
+        systemOutPrintln(secondFileName);
+    }
+
+    public static void systemOutPrintln(String fileName) throws InterruptedException {
+        ReadFileInterface f = new ReadFileThread();
+        f.setFileName(fileName);
+        f.start();
+        f.join();
+        //add your code here - добавьте код тут
+        System.out.println(f.getFileContent());
+    }
+
+    public interface ReadFileInterface {
+
+        void setFileName(String fullFileName);
+
+        String getFileContent();
+
+        void join() throws InterruptedException;
+
+        void start();
+    }
+
+    public static class ReadFileThread extends Thread implements ReadFileInterface {
+        String fileName;
+        String fileContent = "";
+        @Override
+        public void setFileName(String fullFileName) {
+            fileName = fullFileName;
+        }
+
+        @Override
+        public String getFileContent() {
+            return fileContent;
+        }
+
+        @Override
+        public void start() {
+            this.run();
+        }
+
+        public void run() {
+            try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
+//                StringBuilder builder = new StringBuilder();
+                String s;
+                while ((s = reader.readLine()) != null) {
+                    fileContent += s+" ";
+                }
+                reader.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    //add your code here - добавьте код тут
+}
