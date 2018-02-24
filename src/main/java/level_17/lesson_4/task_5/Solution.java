1e2cbae007fa144c1572be66993d04832013056a
Ivan Korol <korol.vanja2008@gmail.com>
2018-02-24 10:23:02 +0200

    compleated level_17 lesson_4 Task_5

---
+++ src/main/java/level_17/lesson_4/task_5/Solution.java
@@ -0,0 +1,9 @@
+package level_17.lesson_4.task_5;
+
+public class Solution {
+    public static void main(String[] args) {
+        IMF fund = IMF.getFund();
+        IMF anotherFund = IMF.getFund();
+        System.out.println(fund == anotherFund );
+    }
+}
