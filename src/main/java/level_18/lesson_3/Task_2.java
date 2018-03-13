453d9121918101e6acdbabff219fbbef1cf43a0b
Ivan Korol <korol.vanja2008@gmail.com>
2018-03-13 18:19:05 +0200

    completed level 18 lesson 3 task 2

---
+++ src/main/java/level_18/lesson_3/Task_2.java
@@ -0,0 +1,26 @@
+package level_18.lesson_3;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(s);
+        int sum = 0;
+        if(fileInputStream.available()>0){
+            sum = fileInputStream.read();
+        }
+        while (fileInputStream.available() > 0){
+            int data = fileInputStream.read();
+            if (data < sum) {
+                sum = data;
+            }
+        }
+        fileInputStream.close();
+        System.out.println(sum);
+    }
+}
