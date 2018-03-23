ef8792df1a091f5aca7f3d0c26584ad2ede7da16
Ivan Korol <korol.vanja2008@gmail.com>
2018-03-23 17:18:08 +0200

    completed level 18 lesson 5 task 5

---
+++ src/main/java/level_18/lesson_5/Task_5.java
@@ -0,0 +1,33 @@
+package level_18.lesson_5;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task_5 {
+    public static void main(String[] args) throws DownloadException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream inputStream;
+        byte[] buff = null;
+        while (true) {
+            try {
+                String file = reader.readLine();
+                inputStream = new FileInputStream(file);
+                buff = new byte[inputStream.available()];
+                System.out.println(buff.length);
+                if (buff.length < 1000) {
+                    reader.close();
+                    inputStream.close();
+                    throw new DownloadException();
+                }
+            } catch (IOException e) {
+               // e.printStackTrace();
+            }
+        }
+    }
+
+    public static class DownloadException extends Exception {
+
+    }
+}
