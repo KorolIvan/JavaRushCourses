fcc94724a4a9ff229306dbc36c7728a06bb3e3da
Ivan Korol <korol.vanja2008@gmail.com>
2018-03-30 17:38:58 +0300

    completed level 18 lesson 8 task 2

---
+++ src/main/java/level_18/lesson_8/task_2/QuestionFileOutputStream.java
@@ -0,0 +1,45 @@
+package level_18.lesson_8.task_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class QuestionFileOutputStream implements AmigoOutputStream {
+    private AmigoOutputStream amigoOutputStream;
+
+    public QuestionFileOutputStream(AmigoOutputStream amigoOutputStream) {
+        this.amigoOutputStream = amigoOutputStream;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        amigoOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        amigoOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String ansver = reader.readLine();
+        if(!ansver.isEmpty() && ansver.equalsIgnoreCase("д")) {
+            amigoOutputStream.close();
+            //System.out.println("closed");
+        }
+        reader.close();
+    }
+}
