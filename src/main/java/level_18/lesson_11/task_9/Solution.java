3460fd7141936710a61a770e7206619998a14771
Ivan Korol <korol.vanja2008@gmail.com>
2018-04-18 08:49:47 +0300

    refactoring after pull request

--- src/main/java/level_18/lesson_11/task_9/Solution.java
+++ src/main/java/level_18/lesson_11/task_9/Solution.java
@@ -1,7 +1,18 @@
 package level_18.lesson_11.task_9;
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        while(true) {
+            String fileName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+            try {
+                FileInputStream inputStream = new FileInputStream(fileName);
+                inputStream.close();
+            }catch (FileNotFoundException f) {
+                System.out.println(fileName);
+                break;
+            }
+        }
     }
 }
