ea6b5b3578cd2d3c8ff9e0a363d6ebfae63247dc
Ivan Korol <korol.vanja2008@gmail.com>
2018-04-18 13:50:40 +0300

    completed level 18 lesson 11 task 11

---
+++ src/main/java/level_18/lesson_11/task_11/Solution.java
@@ -0,0 +1,58 @@
+package level_18.lesson_11.task_11;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        byte[] key = new byte[] {1,2,3,4,5};
+
+        switch (args[0]) {
+            case "-e": {
+                FileInputStream src = new FileInputStream(args[1]);
+                FileOutputStream dst = new FileOutputStream(args[2]);
+
+                while (src.available() > 0) {
+                    byte[] buf = new byte[src.available()];
+                    src.read(buf);
+
+                    //XOR Code
+                    int j = 0;
+                    for (int i = 0; i < buf.length; i++) {
+                        buf[i] = (byte) (buf[i] ^ key[j]);
+                        if (j > key.length)
+                            j = 0;
+                    }
+                    dst.write(buf);
+                }
+                src.close();
+                dst.close();
+                break;
+            }
+            case "-d": {
+                FileInputStream src = new FileInputStream(args[1]);
+                FileOutputStream dst = new FileOutputStream(args[2]);
+
+                while (src.available() > 0) {
+                    byte[] buf = new byte[src.available()];
+                    src.read(buf);
+
+                    //XOR deCode
+                    int j = 0;
+                    for (int i = 0; i < buf.length; i++) {
+                        buf[i] = (byte) (buf[i] ^ key[j]);
+                        if (j > key.length)
+                            j = 0;
+                    }
+                    dst.write(buf);
+                }
+                src.close();
+                dst.close();
+                break;
+            }
+        }
+
+    }
+
+}
