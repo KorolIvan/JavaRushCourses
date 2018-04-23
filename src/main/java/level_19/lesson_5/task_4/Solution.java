333945a47d6656d4419e1c9b50f7daca90bba003
Ivan Korol <korol.vanja2008@gmail.com>
2018-04-23 18:15:37 +0300

    completed level 19 lesson 5 task 4

---
+++ src/main/java/level_19/lesson_5/task_4/Solution.java
@@ -0,0 +1,24 @@
+package level_19.lesson_5.task_4;
+
+import java.io.*;
+
+/**
+ * @author Ivan Korol on 4/23/2018
+ */
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new  BufferedReader(new InputStreamReader(System.in));
+        String fileOne = reader.readLine();
+        String fileTwo = reader.readLine();
+        reader.close();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileOne));
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileTwo));
+        while (fileReader.ready()) {
+            String s = fileReader.readLine();
+            fileWriter.write(s.replace(".", "!"));
+        }
+        fileReader.close();
+        fileWriter.close();
+    }
+}
