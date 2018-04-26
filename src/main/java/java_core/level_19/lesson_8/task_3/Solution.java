568ac04170a93b2b10685c48f1fe4ce31e7e3f74
Ivan Korol <korol.vanja2008@gmail.com>
2018-04-26 14:36:26 +0300

    java_core complete level 19 lesson 8 task 3

---
+++ src/main/java/java_core/level_19/lesson_8/task_3/Solution.java
@@ -0,0 +1,28 @@
+package java_core.level_19.lesson_8.task_3;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * @author Ivan Korol on 4/26/2018
+ */
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString().replaceAll("\\D", "");
+        System.setOut(consoleStream);
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
