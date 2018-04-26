28cb264633fb21cf791b823557d8e23b842281a1
Ivan Korol <korol.vanja2008@gmail.com>
2018-04-26 15:22:38 +0300

    java_core complete level 19 lesson 8 task 5

---
+++ src/main/java/java_core/level_19/lesson_8/task_5/Solution.java
@@ -0,0 +1,34 @@
+package java_core.level_19.lesson_8.task_5;
+
+import java.io.*;
+
+/**
+ * @author Ivan Korol on 4/26/2018
+ */
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file = reader.readLine();
+        reader.close();
+        PrintStream console = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+        System.setOut(console);
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        fileOutputStream.write(result.getBytes());
+        fileOutputStream.close();
+        System.out.println(result);
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
