0de7137134fbed04949bc0d5b83b48ac854aa3db
Ivan Korol <korol.vanja2008@gmail.com>
2018-04-30 14:04:33 +0300

    java_core complete level 20 lesson 2 task 5

---
+++ src/main/java/java_core/level_20/lesson_2/task_5/Asset.java
@@ -0,0 +1,47 @@
+package java_core.level_20.lesson_2.task_5;
+
+/**
+ * @author Ivan Korol on 4/30/2018
+ */
+public class Asset {
+    public Asset(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Asset asset = (Asset) o;
+
+        if (Double.compare(asset.price, price) != 0) return false;
+        return name != null ? name.equals(asset.name) : asset.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
