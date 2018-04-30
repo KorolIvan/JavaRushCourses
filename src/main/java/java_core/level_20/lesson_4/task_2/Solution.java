bc28bbb90840a02ecf4ca75adbff276ad6339803
Ivan Korol <korol.vanja2008@gmail.com>
2018-04-30 17:45:18 +0300

    java_core complete level 20 lesson 4 task 2

---
+++ src/main/java/java_core/level_20/lesson_4/task_2/Solution.java
@@ -0,0 +1,18 @@
+package java_core.level_20.lesson_4.task_2;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ivan Korol on 4/30/2018
+ */
+public class Solution {
+    public static class JavaRush implements Serializable{
+        public List<User> users = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/java_core/level_20/lesson_4/task_2/User.java
@@ -0,0 +1,70 @@
+package java_core.level_20.lesson_4.task_2;
+
+import java.util.Date;
+
+/**
+ * @author Ivan Korol on 4/30/2018
+ */
+public class User {
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private boolean isMale;
+    private Country country;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public static enum Country {
+        UKRAINE("Ukraine"),
+        RUSSIA("Russia"),
+        OTHER("Other");
+
+        private String name;
+
+        private Country(String name) {
+            this.name = name;
+        }
+
+        public String getDisplayedName() {
+            return this.name;
+        }
+    }
+}
