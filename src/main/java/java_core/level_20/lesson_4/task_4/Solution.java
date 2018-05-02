0f413b3d12efe847adcc8fcd12218c7a1c836e40
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-02 09:08:04 +0300

    java_core complete level 20 lesson 4 task 4

---
+++ src/main/java/java_core/level_20/lesson_4/task_4/Solution.java
@@ -0,0 +1,18 @@
+package java_core.level_20.lesson_4.task_4;
+
+import java.io.Serializable;
+
+/**
+ * @author Ivan Korol on 5/2/2018
+ */
+public class Solution {
+    public static class ClassWithStatic implements Serializable {
+        public static String staticString = "it's test static string";
+        public int i;
+        public int j;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
