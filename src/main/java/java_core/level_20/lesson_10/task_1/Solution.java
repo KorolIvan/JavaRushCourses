0f33de865a6a0c56e7005ec64c13c0cd9f80e3ca
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-03 18:04:22 +0300

    java_core complete level 20 lesson 10 task 1

---
+++ src/main/java/java_core/level_20/lesson_10/task_1/Solution.java
@@ -0,0 +1,43 @@
+package java_core.level_20.lesson_10.task_1;
+
+import java.io.Serializable;
+
+/**
+ * @author Ivan Korol on 5/3/2018
+ */
+public class Solution {
+    public class A implements Serializable {
+        String name = "A";
+
+        public A(String name) {
+            this.name += name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    public class B extends A {
+        String name = "B";
+
+        public B(String name) {
+            super(name);
+            this.name += name;
+        }
+    }
+
+    public class C extends B {
+        String name = "C";
+
+        public C(String name) {
+            super(name);
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
