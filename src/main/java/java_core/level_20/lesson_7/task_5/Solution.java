fa28decb6b0b0453e8bda9dd2cbf93c690893564
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-03 16:51:10 +0300

    java_core complete level 20 lesson 7 task 5

---
+++ src/main/java/java_core/level_20/lesson_7/task_5/Solution.java
@@ -0,0 +1,45 @@
+package java_core.level_20.lesson_7.task_5;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * @author Ivan Korol on 5/3/2018
+ */
+public class Solution implements Serializable, Runnable{
+    transient private Thread runner;
+    private int speed;
+
+    public Solution(int speed) {
+        this.speed = speed;
+        runner = new Thread(this);
+        runner.start();
+    }
+
+    public void run() {
+        // do something here, does not matter
+    }
+
+    /**
+     Переопределяем сериализацию.
+     Для этого необходимо объявить методы:
+     private void writeObject(ObjectOutputStream out) throws IOException
+     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
+     Теперь сериализация/десериализация пойдет по нашему сценарию :)
+     */
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        runner = new Thread(this);
+        runner.start();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
