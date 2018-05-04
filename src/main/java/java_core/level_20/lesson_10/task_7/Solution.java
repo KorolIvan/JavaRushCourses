cec03a6a198c476f9e65e065071bcce5896f936f
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-04 13:48:39 +0300

    java_core complete level 20 lesson 10 task 7

---
+++ src/main/java/java_core/level_20/lesson_10/task_7/Solution.java
@@ -0,0 +1,41 @@
+package java_core.level_20.lesson_10.task_7;
+
+import java.io.*;
+
+/**
+ * @author Ivan Korol on 5/4/2018
+ */
+public class Solution implements Serializable, AutoCloseable {
+    transient private FileOutputStream stream;
+    private String fileName;
+
+    public Solution(String fileName) throws FileNotFoundException {
+        this.stream = new FileOutputStream(fileName);
+        this.fileName = fileName;
+    }
+
+    public void writeObject(String string) throws IOException {
+        stream.write(string.getBytes());
+        stream.write("\n".getBytes());
+        stream.flush();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        stream = new FileOutputStream(fileName, true);
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Closing everything!");
+        stream.close();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
