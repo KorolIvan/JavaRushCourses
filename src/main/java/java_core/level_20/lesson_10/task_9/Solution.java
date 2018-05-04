f532d07d00fd9cb20b4ac9532b2ee4de2d090a9e
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-04 14:00:23 +0300

    java_core complete level 20 lesson 10 task 9

---
+++ src/main/java/java_core/level_20/lesson_10/task_9/Solution.java
@@ -0,0 +1,17 @@
+package java_core.level_20.lesson_10.task_9;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Ivan Korol on 5/4/2018
+ */
+public class Solution implements Serializable{
+    int node;
+    List<Solution> edges = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+    }
+}
