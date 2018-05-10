bac8bc920efe96579e9aa6a24ef451b5d58ead27
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-10 13:39:20 +0300

    java_collection level 1 lesson 15 task 1

---
+++ src/main/java/Java_Collections/level_1/lesson_15/task_1/CustomTree.java
@@ -0,0 +1,21 @@
+package Java_Collections.level_1.lesson_15.task_1;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+
+/**
+ * @author Ivan Korol on 5/10/2018
+ */
+public class CustomTree extends AbstractList<String> implements Cloneable, Serializable{
+
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/main/java/Java_Collections/level_1/lesson_15/task_1/Solution.java
@@ -0,0 +1,12 @@
+package Java_Collections.level_1.lesson_15.task_1;
+
+import java.util.List;
+
+/**
+ * @author Ivan Korol on 5/10/2018
+ */
+public class Solution {
+    public static void main(String[] args) {
+        List<String> list = new CustomTree();
+    }
+}
