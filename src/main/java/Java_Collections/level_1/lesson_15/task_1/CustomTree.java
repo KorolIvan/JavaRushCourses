305040302f467cd379e92f324f1a9622e84ad998
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-10 13:55:30 +0300

    java_collection level 1 lesson 15 task 2

--- src/main/java/Java_Collections/level_1/lesson_15/task_1/CustomTree.java
+++ src/main/java/Java_Collections/level_1/lesson_15/task_1/CustomTree.java
@@ -2,6 +2,9 @@ package Java_Collections.level_1.lesson_15.task_1;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Ivan Korol on 5/10/2018
@@ -10,8 +13,46 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
 
     @Override
-    public String get(int index) {
-        return null;
+    public String get(int index) throws UnsupportedOperationException{
+
+        throw new UnsupportedOperationException();
+        //return null;
+    }
+
+    @Override
+    public String set(int index, String element) {
+        //return super.set(index, element);
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, String element) {
+        //super.add(index, element);
+         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index) {
+        //return super.remove(index);
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) {
+        //return super.subList(fromIndex, toIndex);
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        //super.removeRange(fromIndex, toIndex);
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) {
+//        return super.addAll(index, c);
+        throw new UnsupportedOperationException();
     }
 
     @Override
