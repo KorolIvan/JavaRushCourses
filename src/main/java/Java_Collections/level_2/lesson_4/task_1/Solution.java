c32cb777e3293c4877640c88fe783cd44dd4c209
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-12 16:21:04 +0300

    completed java collection level_2 lesson_4 Task_1

---
+++ src/main/java/Java_Collections/level_2/lesson_4/task_1/Solution.java
@@ -0,0 +1,17 @@
+package Java_Collections.level_2.lesson_4.task_1;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = getStackTrace(new IndexOutOfBoundsException("fff"));
+        System.out.println(text);
+    }
+
+    public static String getStackTrace(Throwable throwable) {
+        StringWriter stringWriter = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
+    }
+}
