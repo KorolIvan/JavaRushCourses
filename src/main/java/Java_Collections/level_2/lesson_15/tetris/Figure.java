b720a3072b7258a934f7608fc7bd351c9475564c
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-23 16:14:03 +0300

    java_collection level 2 lesson 15 (tetris) task 8

--- src/main/java/Java_Collections/level_2/lesson_15/tetris/Figure.java
+++ src/main/java/Java_Collections/level_2/lesson_15/tetris/Figure.java
@@ -4,4 +4,25 @@ package Java_Collections.level_2.lesson_15.tetris;
  * @author Ivan Korol on 5/23/2018
  */
 public class Figure {
+    private int x;
+    private int y;
+    private int[][] matrix;
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
