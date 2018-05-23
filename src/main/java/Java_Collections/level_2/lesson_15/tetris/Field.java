35900625bf1425dad6cecc5763fcd103ea4e8d51
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-23 17:05:43 +0300

    java_collection level 2 lesson 15 (tetris) task 13

--- src/main/java/Java_Collections/level_2/lesson_15/tetris/Field.java
+++ src/main/java/Java_Collections/level_2/lesson_15/tetris/Field.java
@@ -26,8 +26,19 @@ public class Field {
         return matrix;
     }
 
+
     public void print() {
 
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j <= width; j++) {
+                if (matrix[i][j] == 0) {
+                    System.out.print(".");
+                }else {
+                    System.out.print("X");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public void removeFullLines(){
