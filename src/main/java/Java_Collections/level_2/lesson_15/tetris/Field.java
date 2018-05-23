31700930d6eef73a1b948da851abc360616f23ab
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-23 16:07:05 +0300

    java_collection level 2 lesson 15 (tetris) task 6

--- src/main/java/Java_Collections/level_2/lesson_15/tetris/Field.java
+++ src/main/java/Java_Collections/level_2/lesson_15/tetris/Field.java
@@ -4,4 +4,25 @@ package Java_Collections.level_2.lesson_15.tetris;
  * @author Ivan Korol on 5/23/2018
  */
 public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public Field(int x, int y) {
+        this.width = x;
+        this.height = y;
+        matrix = new int[y][x];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
