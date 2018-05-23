ea58e47cb6e855d21fe8fed3902a0d1dab9c5f81
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-23 14:03:12 +0300

    java_collection level 2 lesson 15 (tetris) task 3

--- src/main/java/Java_Collections/level_2/lesson_15/tetris/Tetris.java
+++ src/main/java/Java_Collections/level_2/lesson_15/tetris/Tetris.java
@@ -7,7 +7,24 @@ public class Tetris {
     private Field field;
     private Figure figure;
 
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public void setFigure(Figure figure) {
+        this.figure = figure;
+    }
+
     public static void main(String[] args) {
+
 
     }
 }
