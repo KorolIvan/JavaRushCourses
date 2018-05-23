bf915759b65640fce122ed22f486605de256c67d
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-23 14:00:06 +0300

    java_collection level 2 lesson 15 (tetris) task 1

---
+++ src/main/java/Java_Collections/level_2/lesson_15/tetris/Field.java
@@ -0,0 +1,7 @@
+package Java_Collections.level_2.lesson_15.tetris;
+
+/**
+ * @author Ivan Korol on 5/23/2018
+ */
+public class Field {
+}
---
+++ src/main/java/Java_Collections/level_2/lesson_15/tetris/Tetris.java
@@ -0,0 +1,10 @@
+package Java_Collections.level_2.lesson_15.tetris;
+
+/**
+ * @author Ivan Korol on 5/23/2018
+ */
+public class Tetris {
+    public static void main(String[] args) {
+
+    }
+}
