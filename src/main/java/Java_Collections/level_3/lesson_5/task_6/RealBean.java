df0a2b7c9aead8c053296b9e24cfb03d63f260b0
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-24 17:11:10 +0300

    java_collection level 3 lesson 5 task 6

---
+++ src/main/java/Java_Collections/level_3/lesson_5/task_6/RealBean.java
@@ -0,0 +1,40 @@
+package Java_Collections.level_3.lesson_5.task_6;
+
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ivan Korol on 5/24/2018
+ */
+public class RealBean {
+    protected final int id;
+    protected final String name;
+
+    protected Map<String, Object> additionalMap = new HashMap<>();
+
+    @JsonCreator
+    public RealBean(@JsonProperty("id") int id, @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalMap() {
+        return additionalMap;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalMap(String name, Object value) {
+        additionalMap.put(name, value);
+    }
+}
---
+++ src/main/java/Java_Collections/level_3/lesson_5/task_6/Solution.java
@@ -0,0 +1,21 @@
+package Java_Collections.level_3.lesson_5.task_6;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+/**
+ * @author Ivan Korol on 5/24/2018
+ */
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringWriter stringWriter = new StringWriter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String sampleJsonString = "{\"id\":1,\"name\":\"first\",\"KEY#1\":\"VALUE#1\",\"KEY#3\":\"VALUE#3\",\"KEY#2\":\"VALUE#2\"}";
+        RealBean realBean = objectMapper.readValue(sampleJsonString, RealBean.class);
+
+        objectMapper.writeValue(stringWriter, realBean);
+        System.out.println(stringWriter.toString());
+    }
+}
