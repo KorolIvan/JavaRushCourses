adb91fab272e387a5f399c6211f6af7ebb2fda5c
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-31 14:44:47 +0300

    java_collection level 3 lesson 15 space game task_12

--- src/main/java/Java_Collections/level_3/lesson_15/space/SpaceShip.java
+++ src/main/java/Java_Collections/level_3/lesson_15/space/SpaceShip.java
@@ -4,7 +4,16 @@ package Java_Collections.level_3.lesson_15.space;
  * @author Ivan Korol on 5/31/2018
  */
 public class SpaceShip extends BaseObject {
-    public SpaceShip(double x, double y, double radius) {
-        super(x, y, radius);
+    private double dx = 0.0;
+    public SpaceShip(double x, double y) {
+        super(x, y, 3);
+    }
+
+    public void moveLeft() {
+        dx = -1;
+    }
+
+    public void moveRight() {
+        dx = 1;
     }
 }
