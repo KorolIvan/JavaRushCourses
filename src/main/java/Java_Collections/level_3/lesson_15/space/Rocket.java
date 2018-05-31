b226be500d7c1571c3f81666f4d7e32f087d605d
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-31 14:40:55 +0300

    java_collection level 3 lesson 15 space game task_11

--- src/main/java/Java_Collections/level_3/lesson_15/space/Rocket.java
+++ src/main/java/Java_Collections/level_3/lesson_15/space/Rocket.java
@@ -4,7 +4,16 @@ package Java_Collections.level_3.lesson_15.space;
  * @author Ivan Korol on 5/31/2018
  */
 public class Rocket extends BaseObject {
-    public Rocket(double x, double y, double radius) {
-        super(x, y, radius);
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
+
+    @Override
+    public void move() {
+        setY(getY()-1);
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.setPoint(getX(), getY(), 'R');
     }
 }
