4fe68c5fefe2441db4ba24d91ff84886b5f95ee8
Ivan Korol <korol.vanja2008@gmail.com>
2018-05-31 13:07:41 +0300

    java_collection level 3 lesson 15 space game

---
+++ src/main/java/Java_Collections/level_3/lesson_15/space/Space.java
@@ -0,0 +1,10 @@
+package Java_Collections.level_3.lesson_15.space;
+
+/**
+ * @author Ivan Korol on 5/31/2018
+ */
+public class Space {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Java_Collections/level_3/lesson_15/space/Ufo.java
@@ -0,0 +1,7 @@
+package Java_Collections.level_3.lesson_15.space;
+
+/**
+ * @author Ivan Korol on 5/31/2018
+ */
+public class Ufo {
+}
