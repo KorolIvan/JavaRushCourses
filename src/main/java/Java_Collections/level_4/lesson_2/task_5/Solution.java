451fc8cfe2ed1a0c9b6f74ca0ae37bdb50dc7bb6
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-05 12:40:25 +0300

    java_collection level 4 lesson 4 task 1

---
+++ src/main/java/Java_Collections/level_4/lesson_2/task_5/Solution.java
@@ -0,0 +1,20 @@
+package Java_Collections.level_4.lesson_2.task_5;
+
+/**
+ * @author Ivan Korol on 5/31/2018
+ */
+//todo make this task
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.recursion("sin(2*(-5+1.5*4)+28)", 0); //expected output 0.5 6
+    }
+
+    public void recursion(final String expression, int countOperation) {
+        //implement
+    }
+
+    public Solution() {
+        //don't delete
+    }
+}
