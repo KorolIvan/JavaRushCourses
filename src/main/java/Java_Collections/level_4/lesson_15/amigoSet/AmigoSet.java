60d1ba0804bc3e05d25115fdebf8a8403fea7a70
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-08 17:26:51 +0300

    java_collection level 4 lesson 15 (AmigoSet) task 2/2

--- src/main/java/Java_Collections/level_4/lesson_15/amigoSet/AmigoSet.java
+++ src/main/java/Java_Collections/level_4/lesson_15/amigoSet/AmigoSet.java
@@ -1,17 +1,42 @@
 package Java_Collections.level_4.lesson_15.amigoSet;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Ivan Korol on 6/8/2018
  */
-public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable, Set {
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        map = new HashMap();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        map = new HashMap<>(Math.max((int) (collection.size() / .75f) + 1, 16));
+        addAll(collection);
+    }
+
     @Override
     public Iterator iterator() {
         return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return super.remove(o);
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return super.addAll(c);
     }
 
     @Override
