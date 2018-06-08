5d9a40e16844576f00a5b59eaff8896312f4452d
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-08 16:22:22 +0300

    java_collection level 4 lesson 15 (AmigoSet) task 2

---
+++ src/main/java/Java_Collections/level_4/lesson_15/amigoSet/AmigoSet.java
@@ -0,0 +1,21 @@
+package Java_Collections.level_4.lesson_15.amigoSet;
+
+import java.io.Serializable;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author Ivan Korol on 6/8/2018
+ */
+public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable, Set {
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
