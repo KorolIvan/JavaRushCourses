36549158dcdc59118d272b3120ebe43c99d83888
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-11 13:22:03 +0300

    java_collection level 5 lesson 4 task 1

---
+++ src/main/java/Java_Collections/level_5/lesson_4/task_1/Solution.java
@@ -0,0 +1,15 @@
+package Java_Collections.level_5.lesson_4.task_1;
+
+import java.util.List;
+
+/**
+ * @author Ivan Korol on 6/11/2018
+ */
+public class Solution<T extends List<Solution.SomeClass>> {
+    public static class SomeClass<T extends Number> {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
