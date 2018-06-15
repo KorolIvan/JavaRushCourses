93133a6a6761cf2593fe6f57bede9b95771cde93
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-15 13:42:58 +0300

    java_collection level 6 lesson 6 task 1

---
+++ src/main/java/Java_Collections/level_6/lesson_6/task_1/Solution.java
@@ -0,0 +1,45 @@
+package Java_Collections.level_6.lesson_6.task_1;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Ivan Korol on 6/15/2018
+ */
+public class Solution {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() throws IllegalAccessException, InstantiationException {
+        Class[] classes = Collections.class.getDeclaredClasses();
+        for (Class c : classes){
+
+            if(Modifier.isPrivate(c.getModifiers()))
+                if(Modifier.isStatic(c.getModifiers()))
+                {
+                    if(List.class.isAssignableFrom(c))
+                    {
+                        try{
+                            Constructor constructor = c.getDeclaredConstructor();
+                            constructor.setAccessible(true);
+                            List list = (List) constructor.newInstance();
+                            list.get(0);
+                        }catch (IndexOutOfBoundsException e){
+                            // вернуть класс
+                            return c;
+                        } catch (NoSuchMethodException e) {
+                            //e.printStackTrace();
+                        } catch (InvocationTargetException e) {
+                            //e.printStackTrace();
+                        }
+                    }
+
+                }
+        }
+        return null;
+    }
+}
