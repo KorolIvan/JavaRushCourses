31d9211e4cfe8532989065d0ceebd780567cc0ab
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-15 17:06:01 +0300

    java_collection level 6 lesson 15 task Shortener 1

---
+++ src/main/java/Java_Collections/level_6/lesson_15/shortener/Shortener.java
@@ -0,0 +1,14 @@
+package Java_Collections.level_6.lesson_15.shortener;
+
+/**
+ * @author Ivan Korol on 6/15/2018
+ */
+public class Shortener {
+    public Long getId(String string){
+        return null;
+    }
+
+    public String getString(Long id) {
+        return null;
+    }
+}
---
+++ src/main/java/Java_Collections/level_6/lesson_15/shortener/Solution.java
@@ -0,0 +1,10 @@
+package Java_Collections.level_6.lesson_15.shortener;
+
+/**
+ * @author Ivan Korol on 6/15/2018
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
