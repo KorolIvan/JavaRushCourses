d2022d9759e5b6c1710ff4edb12cb0f6a99fd18c
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-15 15:44:16 +0300

    java_collection level 6 lesson 10 task 1

---
+++ src/main/java/Java_Collections/level_6/lesson_10/Solution.java
@@ -0,0 +1,16 @@
+package Java_Collections.level_6.lesson_10;
+
+import java.util.concurrent.DelayQueue;
+
+/**
+ * @author Ivan Korol on 6/15/2018
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        return DelayQueue.class;
+    }
+}
