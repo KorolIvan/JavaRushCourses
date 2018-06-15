32d332c8dc183515684a5c48dcb8e9b730280838
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-15 14:36:48 +0300

    java_collection level 6 lesson 8 task 1

---
+++ src/main/java/Java_Collections/level_6/lesson_8/task_1/Solution.java
@@ -0,0 +1,10 @@
+package Java_Collections.level_6.lesson_8.task_1;
+
+/**
+ * @author Ivan Korol on 6/15/2018
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
