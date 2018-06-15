f38380f271fa1950057429e264897c45f5453284
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-15 17:37:06 +0300

    java_collection level 6 lesson 15 task Shortener 3

--- src/main/java/Java_Collections/level_6/lesson_15/shortener/Shortener.java
+++ src/main/java/Java_Collections/level_6/lesson_15/shortener/Shortener.java
@@ -1,14 +1,28 @@
 package Java_Collections.level_6.lesson_15.shortener;
 
+import Java_Collections.level_6.lesson_15.shortener.strategy.StorageStrategy;
+
 /**
  * @author Ivan Korol on 6/15/2018
  */
 public class Shortener {
-    public Long getId(String string){
-        return null;
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id) {
-        return null;
+    public synchronized Long getId(String string) {
+        if (!storageStrategy.containsValue(string)) {
+            lastId += 1;
+            storageStrategy.put(lastId, string);
+        }
+
+        return storageStrategy.getKey(string);
+    }
+
+    public synchronized String getString(Long id) {
+        return storageStrategy.getValue(id);
     }
 }
