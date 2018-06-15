c4a847b2cb1b5837b4eb56447e3ccb9d0ffdbfa3
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-15 15:53:54 +0300

    java_collection level 6 lesson 10 task 3

---
+++ src/main/java/Java_Collections/level_6/lesson_10/task_3/Solution.java
@@ -0,0 +1,16 @@
+package Java_Collections.level_6.lesson_10.task_3;
+
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * @author Ivan Korol on 6/15/2018
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        return SynchronousQueue.class;
+    }
+}
