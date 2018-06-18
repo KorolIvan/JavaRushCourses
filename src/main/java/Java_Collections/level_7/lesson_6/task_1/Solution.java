110ec6055614a1409f9cc2974b92a17cb7aa315e
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-18 16:01:35 +0300

    com.javarush.task.task37.task3703; done

---
+++ src/main/java/Java_Collections/level_7/lesson_6/task_1/Solution.java
@@ -0,0 +1,16 @@
+package Java_Collections.level_7.lesson_6.task_1;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+
+/**
+ * @author Ivan Korol on 6/18/2018
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        return ConcurrentSkipListMap.class;
+    }
+}
