abd5236186c2260e222b5c097ae0cd730c656359
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-20 18:08:43 +0300

    com.javarush.task.task39.task3913 part 8 done/test

--- src/main/java/Java_Collections/level_7/lesson_15/bigparser/Solution.java
+++ src/main/java/Java_Collections/level_7/lesson_15/bigparser/Solution.java
@@ -9,9 +9,11 @@ import java.nio.file.Paths;
 public class Solution {
     public static void main(String[] args) {
         LogParser logParser = new LogParser(Paths.get("C:\\documents\\javarush\\JavaRushCourses\\src\\main\\java\\Java_Collections\\level_7\\lesson_15\\bigparser\\logs\\"));
-        //System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
-//        System.out.println(logParser.getNumberOfUniqueIPs( new Date(), null));
-        System.out.println(logParser.getNumberOfUniqueIPs(null, null));
-//        System.out.println(logParser.getNumberOfUniqueIPs(new Date(), new Date()));
+        System.out.println(logParser.execute("get date for event = \"LOGIN\" and date between \"11.12.2013 0:00:00\" and \"07.01.2021 20:22:55\""));
+        System.out.println(logParser.execute("get ip for status = \"ERROR\" and date between \"03.11.2014 12:56:22\" and \"29.2.31020 5:4:8\""));
+        System.out.println(logParser.execute("get date"));
+        System.out.println(logParser.execute("get event"));
+        System.out.println(logParser.execute("get status"));
+        System.out.println(logParser.execute("get ip"));
     }
 }
