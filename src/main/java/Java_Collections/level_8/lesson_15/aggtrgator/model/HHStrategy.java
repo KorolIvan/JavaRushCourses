01cbaeb9cb3a3ac5238da55d399e763385f10b35
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-22 11:20:11 +0300

    com.javarush.task.task28.task2810 part 3 done

---
+++ src/main/java/Java_Collections/level_8/lesson_15/aggtrgator/model/HHStrategy.java
@@ -0,0 +1,7 @@
+package Java_Collections.level_8.lesson_15.aggtrgator.model;
+
+/**
+ * @author Ivan Korol on 6/22/2018
+ */
+public class HHStrategy implements Strategy{
+}
--- src/main/java/Java_Collections/level_8/lesson_15/aggtrgator/model/Provider.java
+++ src/main/java/Java_Collections/level_8/lesson_15/aggtrgator/model/Provider.java
@@ -1,5 +1,9 @@
 package Java_Collections.level_8.lesson_15.aggtrgator.model;
 
+import Java_Collections.level_8.lesson_15.aggtrgator.vo.Vacancy;
+
+import java.util.List;
+
 /**
  * @author Ivan Korol on 6/21/2018
  */
@@ -13,6 +17,10 @@ public class Provider {
 
     public void setStrategy(Strategy strategy) {
         this.strategy = strategy;
+    }
+
+    public List<Vacancy> getJavaVacancies(String searchString){
+        return null;
     }
 
 }
