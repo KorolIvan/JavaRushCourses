a7973cf78f4ad0f577c252e2cac48a3286a32af7
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-22 13:47:42 +0300

    com.javarush.task.task28.task2810 part 12 done

--- src/main/java/Java_Collections/level_8/lesson_15/aggtrgator/Aggregator.java
+++ src/main/java/Java_Collections/level_8/lesson_15/aggtrgator/Aggregator.java
@@ -1,10 +1,19 @@
 package Java_Collections.level_8.lesson_15.aggtrgator;
 
+import Java_Collections.level_8.lesson_15.aggtrgator.model.HHStrategy;
+import Java_Collections.level_8.lesson_15.aggtrgator.model.Model;
+import Java_Collections.level_8.lesson_15.aggtrgator.model.Provider;
+import Java_Collections.level_8.lesson_15.aggtrgator.view.HtmlView;
+
 /**
  * @author Ivan Korol on 6/21/2018
  */
 public class Aggregator {
     public static void main(String[] args) {
-
+        HtmlView view = new HtmlView();
+        Model model = new Model(view, new Provider(new HHStrategy()));
+        Controller controller = new Controller(model);
+        view.setController(controller);
+        view.userCitySelectEmulationMethod();
     }
 }
