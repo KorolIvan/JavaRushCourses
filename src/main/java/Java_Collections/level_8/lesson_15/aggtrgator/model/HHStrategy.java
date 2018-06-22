b3585dc7f4f0e96cd0ccdfba9b09a69cd571b4ee
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-22 11:41:28 +0300

    com.javarush.task.task28.task2810 part 4 done

--- src/main/java/Java_Collections/level_8/lesson_15/aggtrgator/model/HHStrategy.java
+++ src/main/java/Java_Collections/level_8/lesson_15/aggtrgator/model/HHStrategy.java
@@ -3,5 +3,18 @@ package Java_Collections.level_8.lesson_15.aggtrgator.model;
 /**
  * @author Ivan Korol on 6/22/2018
  */
-public class HHStrategy implements Strategy{
+public class HHStrategy implements Strategy {
+    private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+    private String city;
+    private int page;
+
+
+    private String getString() {
+        return String.format(URL_FORMAT, city, page);
+    }
+
+    public void setString(String city, int page){
+        this.city = city;
+        this.page = page;
+    }
 }
