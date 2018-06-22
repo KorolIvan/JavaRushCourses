3256a65af38791e3461554b1da0ce9871a147f9a
Ivan Korol <korol.vanja2008@gmail.com>
2018-06-22 14:48:59 +0300

    com.javarush.task.task28.task2810 part 15 done

--- src/main/java/Java_Collections/level_8/lesson_15/aggtrgator/view/HtmlView.java
+++ src/main/java/Java_Collections/level_8/lesson_15/aggtrgator/view/HtmlView.java
@@ -2,6 +2,10 @@ package Java_Collections.level_8.lesson_15.aggtrgator.view;
 
 import Java_Collections.level_8.lesson_15.aggtrgator.Controller;
 import Java_Collections.level_8.lesson_15.aggtrgator.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -19,10 +23,10 @@ public class HtmlView implements View {
     public void update(List<Vacancy> vacancies) {
         System.out.println(vacancies.size());
         System.out.println(filePath);
-        try{
+        try {
             String newFile = getUpdatedFileContent(vacancies);
             updateFile(newFile);
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -37,16 +41,58 @@ public class HtmlView implements View {
     }
 
     private String getUpdatedFileContent(List<Vacancy> vacancies) {
-        return null;
+        Document doc;
+        try {
+            doc = getDocument();
+
+            Elements elementVacancy = doc.getElementsByAttributeValue("class", "vacancy");
+            for (int i = 0; i < elementVacancy.size(); i++) {
+                elementVacancy.get(i).remove();
+            }
+            Element templateElement = doc.getElementsByClass("template").first();
+            Element cloneTemplateElement = templateElement.clone();
+            cloneTemplateElement.removeClass("template");
+            cloneTemplateElement.removeAttr("style");
+
+            Vacancy vacancy;
+
+            for (int i = 0; i < vacancies.size(); i++) {
+                vacancy = vacancies.get(i);
+                Element e = cloneTemplateElement.clone();
+
+                //add city
+                e.getElementsByClass("city").first().text(vacancy.getCity());
+                // add companyName
+                e.getElementsByClass("companyName").first().text(vacancy.getCompanyName());
+                // add salary
+                e.getElementsByClass("salary").first().text(vacancy.getSalary());
+                //add url
+                Element elementLink = e.getElementsByClass("title").first();
+                elementLink.text(vacancy.getTitle());
+                elementLink.attr("href", vacancy.getUrl());
+
+                templateElement.before(e.outerHtml());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Some exception occurred";
+
+
+        }
+        return doc.toString();
     }
 
     private void updateFile(String s) {
-        try{
+        try {
             FileWriter fileWriter = new FileWriter(new File(filePath));
             fileWriter.write(s);
             fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
     }
 }
