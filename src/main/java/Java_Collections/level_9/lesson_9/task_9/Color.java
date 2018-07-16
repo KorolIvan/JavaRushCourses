8e29289b5fb0fa8e0a78a26efef95ebd7c339226
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-16 12:24:57 +0300

    com.javarush.task.task39.task3905 done

---
+++ src/main/java/Java_Collections/level_9/lesson_9/task_9/Color.java
@@ -0,0 +1,14 @@
+package Java_Collections.level_9.lesson_9.task_9;
+
+/**
+ * @author Ivan Korol on 7/16/2018
+ */
+public enum Color {
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    INDIGO,
+    VIOLET
+}
