9519594325f87756c488f872319be107bb957421
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-16 13:33:40 +0300

    cash machine com.javarush.task.task26.task2613 done (1)

--- src/main/java/Java_Collections/level_9/cashMachine/ConsoleHelper.java
+++ src/main/java/Java_Collections/level_9/cashMachine/ConsoleHelper.java
@@ -1,7 +1,20 @@
 package Java_Collections.level_9.cashMachine;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author Ivan Korol on 7/16/2018
  */
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        return bis.readLine();
+    }
 }
---
+++ src/main/java/Java_Collections/level_9/cashMachine/exception/InterruptOperationException.java
@@ -0,0 +1,7 @@
+package Java_Collections.level_9.cashMachine.exception;
+
+/**
+ * @author Ivan Korol on 7/16/2018
+ */
+public class InterruptOperationException extends Exception {
+}
