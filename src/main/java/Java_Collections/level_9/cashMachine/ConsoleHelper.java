564c48c7acb8b6710354954bd01221e0403a28be
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-16 13:23:56 +0300

    cash machine com.javarush.task.task26.task2613 done

---
+++ src/main/java/Java_Collections/level_9/cashMachine/ConsoleHelper.java
@@ -0,0 +1,7 @@
+package Java_Collections.level_9.cashMachine;
+
+/**
+ * @author Ivan Korol on 7/16/2018
+ */
+public class ConsoleHelper {
+}
---
+++ src/main/java/Java_Collections/level_9/cashMachine/Operation.java
@@ -0,0 +1,11 @@
+package Java_Collections.level_9.cashMachine;
+
+/**
+ * @author Ivan Korol on 7/16/2018
+ */
+public enum Operation {
+    INFO,
+    DEPOSIT,
+    WITHDRAW,
+    EXIT
+}
