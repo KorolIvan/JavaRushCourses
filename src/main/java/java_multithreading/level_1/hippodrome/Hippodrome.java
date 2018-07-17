e341b9649cdd2fac22e1c2c513beead66fe97930
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 17:40:49 +0300

    hippodrome com.javarush.task.task21.task2113 done (2)

--- src/main/java/java_multithreading/level_1/hippodrome/Hippodrome.java
+++ src/main/java/java_multithreading/level_1/hippodrome/Hippodrome.java
@@ -1,9 +1,17 @@
 package java_multithreading.level_1.hippodrome;
 
+import java.util.List;
+
 /**
  * @author Ivan Korol on 7/17/2018
  */
 public class Hippodrome {
+    private List<Horse> horses;
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
     public static void main(String[] args) {
 
     }
