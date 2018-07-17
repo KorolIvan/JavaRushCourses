56fdc1ae232ecf6a88426201a4e990c1dcf1c4bc
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 16:53:37 +0300

    com.javarush.task.task21.task2109 done

---
+++ src/main/java/java_multithreading/level_1/lesson_8/task_3/Solution.java
@@ -0,0 +1,60 @@
+package java_multithreading.level_1.lesson_8.task_3;
+
+/**
+ * @author Ivan Korol on 7/17/2018
+ */
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+    }
+
+    public static class C extends B {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        protected C clone() throws CloneNotSupportedException {
+            int i = getI();
+            int j = getJ();
+            String name = getName();
+            return new C(i, j, name);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
