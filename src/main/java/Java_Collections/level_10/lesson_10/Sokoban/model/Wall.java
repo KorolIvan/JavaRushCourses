84de6c177fffc6b8ca6d2f3a8cb990dd1458877b
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 13:19:46 +0300

    Sokoban com.javarush.task.task34.task3410 done (7)

---
+++ src/main/java/Java_Collections/level_10/lesson_10/Sokoban/model/Wall.java
@@ -0,0 +1,21 @@
+package Java_Collections.level_10.lesson_10.Sokoban.model;
+
+import java.awt.*;
+
+/**
+ * @author Ivan Korol on 7/17/2018
+ */
+public class Wall extends CollisionObject {
+    public Wall(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.DARK_GRAY);
+        int leftUpperCornerX = getX() - getWidth() / 2;
+        int leftUpperCornerY = getY() - getHeight() / 2;
+        graphics.drawRect(leftUpperCornerX, leftUpperCornerY, getWidth(), getHeight());
+        graphics.fillRect(leftUpperCornerX, leftUpperCornerY, getWidth(), getHeight());
+    }
+}
