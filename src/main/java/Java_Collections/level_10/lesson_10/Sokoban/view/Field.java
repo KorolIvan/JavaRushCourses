3949af76e18b8eb08ee959e3b788a47c8b6eb71f
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 12:56:36 +0300

    Sokoban com.javarush.task.task34.task3410 done (3)

---
+++ src/main/java/Java_Collections/level_10/lesson_10/Sokoban/view/Field.java
@@ -0,0 +1,19 @@
+package Java_Collections.level_10.lesson_10.Sokoban.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Ivan Korol on 7/17/2018
+ */
+public class Field extends JPanel {
+    private View view;
+    public Field(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
