3503bf3459b75e6857b2457dee40f6515598e5db
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 14:40:27 +0300

    Sokoban com.javarush.task.task34.task3410 done (14)

--- src/main/java/Java_Collections/level_10/lesson_10/Sokoban/view/Field.java
+++ src/main/java/Java_Collections/level_10/lesson_10/Sokoban/view/Field.java
@@ -1,10 +1,13 @@
 package Java_Collections.level_10.lesson_10.Sokoban.view;
 
 import Java_Collections.level_10.lesson_10.Sokoban.controller.EventListener;
+import Java_Collections.level_10.lesson_10.Sokoban.model.Direction;
 import Java_Collections.level_10.lesson_10.Sokoban.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Set;
 
 /**
@@ -16,6 +19,8 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
+        this.addKeyListener(new KeyHandler());
+        this.setFocusable(true);
     }
 
     @Override
@@ -31,4 +36,30 @@ public class Field extends JPanel {
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
     }
+
+    public class KeyHandler extends KeyAdapter {
+        private Field field;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()){
+                case (KeyEvent.VK_LEFT):
+                    eventListener.move(Direction.LEFT);
+                    break;
+                case (KeyEvent.VK_RIGHT):
+                    eventListener.move(Direction.RIGHT);
+                    break;
+                case (KeyEvent.VK_UP):
+                    eventListener.move(Direction.UP);
+                    break;
+                case (KeyEvent.VK_DOWN):
+                    eventListener.move(Direction.DOWN);
+                    break;
+                case (KeyEvent.VK_R):
+                    eventListener.restart();
+                    break;
+            }
+        }
+    }
+
 }
