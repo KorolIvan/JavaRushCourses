f999a010538d69e594896029968650ffa59bd7ac
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 13:51:08 +0300

    Sokoban com.javarush.task.task34.task3410 done (11)

--- src/main/java/Java_Collections/level_10/lesson_10/Sokoban/model/Model.java
+++ src/main/java/Java_Collections/level_10/lesson_10/Sokoban/model/Model.java
@@ -2,14 +2,36 @@ package Java_Collections.level_10.lesson_10.Sokoban.model;
 
 import Java_Collections.level_10.lesson_10.Sokoban.controller.EventListener;
 
+import java.nio.file.Paths;
+
 /**
  * @author Ivan Korol on 7/17/2018
  */
 public class Model {
     public static final int FIELD_CELL_SIZE = 20;
     private EventListener eventListener;
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    private LevelLoader levelLoader = new LevelLoader(Paths.get("..\\res\\levels.txt"));
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(int level) {
+        this.gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel = currentLevel + 1;
+        restartLevel(currentLevel);
     }
 }
