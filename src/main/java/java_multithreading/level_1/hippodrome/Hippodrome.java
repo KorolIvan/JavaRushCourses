7098955a4d175e2abd6eb1f6a4a4806138a0a54d
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 17:38:50 +0300

    hippodrome com.javarush.task.task21.task2113 done (1)

---
+++ src/main/java/java_multithreading/level_1/hippodrome/Hippodrome.java
@@ -0,0 +1,10 @@
+package java_multithreading.level_1.hippodrome;
+
+/**
+ * @author Ivan Korol on 7/17/2018
+ */
+public class Hippodrome {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/java_multithreading/level_1/hippodrome/Horse.java
@@ -0,0 +1,7 @@
+package java_multithreading.level_1.hippodrome;
+
+/**
+ * @author Ivan Korol on 7/17/2018
+ */
+public class Horse {
+}
