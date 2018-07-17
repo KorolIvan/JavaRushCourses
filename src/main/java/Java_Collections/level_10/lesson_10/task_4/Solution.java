165a2030c7661a004bfb1be5a51ae544dd8c91fb
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 12:07:51 +0300

    com.javarush.task.task40.task4012 done

---
+++ src/main/java/Java_Collections/level_10/lesson_10/task_4/Solution.java
@@ -0,0 +1,34 @@
+package Java_Collections.level_10.lesson_10.task_4;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * @author Ivan Korol on 7/17/2018
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isLeap(LocalDate date) {
+        return date.isLeapYear();
+    }
+
+    public static boolean isBefore(LocalDateTime dateTime) {
+        return dateTime.isBefore(LocalDateTime.now());
+    }
+
+    public static LocalTime addTime(LocalTime time, int n, ChronoUnit chronoUnit) {
+        return time.plus(n, chronoUnit);
+    }
+
+    public static Period getPeriodBetween(LocalDate firstDate, LocalDate secondDate) {
+        if (firstDate.isBefore(secondDate)){
+            return Period.between(firstDate, secondDate);
+        }else return Period.between(secondDate, firstDate);
+    }
+}
