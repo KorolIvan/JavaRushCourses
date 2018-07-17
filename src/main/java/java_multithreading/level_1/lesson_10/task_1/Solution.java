1a17c1d63136ea559b483e3bc961cd30641d2671
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 17:07:19 +0300

    com.javarush.task.task21.task2110 done

---
+++ src/main/java/java_multithreading/level_1/lesson_10/task_1/Solution.java
@@ -0,0 +1,42 @@
+package java_multithreading.level_1.lesson_10.task_1;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ * @author Ivan Korol on 7/17/2018
+ */
+public class Solution {
+    public static void writeZipEntriesToFile(String zipFileName, String outputFileName) {
+        Charset charset = StandardCharsets.UTF_8;
+        Path outputFilePath = Paths.get(outputFileName);
+
+        try {
+            try (ZipFile zip = new ZipFile(zipFileName);
+                 BufferedWriter writer = Files.newBufferedWriter(outputFilePath, charset)) {
+
+                String newLine = System.getProperty("line.separator");
+                for (Enumeration entries = zip.entries(); entries.hasMoreElements(); ) {
+                    // Берем имя файла из архива и записываем его в результирующий файл
+                    // Get the entry name and write it to the output file
+                    String zipEntryName = ((ZipEntry) entries.nextElement()).getName() + newLine;
+                    writer.write(zipEntryName, 0, zipEntryName.length());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
