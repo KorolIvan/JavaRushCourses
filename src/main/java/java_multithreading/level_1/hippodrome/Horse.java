3348e28991198124b165e350deb668515d75d5bc
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 17:44:14 +0300

    hippodrome com.javarush.task.task21.task2113 done (4)

--- src/main/java/java_multithreading/level_1/hippodrome/Horse.java
+++ src/main/java/java_multithreading/level_1/hippodrome/Horse.java
@@ -4,4 +4,7 @@ package java_multithreading.level_1.hippodrome;
  * @author Ivan Korol on 7/17/2018
  */
 public class Horse {
+    private String name;
+    private double speed;
+    private double distance;
 }
