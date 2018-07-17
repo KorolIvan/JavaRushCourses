cfa8be412ea2184b08dfd44ec7c6e1fba7455531
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 14:08:13 +0300

    Sokoban com.javarush.task.task34.task3410 done (13)

--- src/main/java/Java_Collections/level_10/lesson_10/Sokoban/controller/Controller.java
+++ src/main/java/Java_Collections/level_10/lesson_10/Sokoban/controller/Controller.java
@@ -16,7 +16,9 @@ public class Controller implements EventListener {
         view = new View(this);
         model = new Model();
         view.init();
+        view.setEventListener(this);
         model.restart();
+        model.setEventListener(this);
     }
 
     public static void main(String[] args) {
@@ -29,21 +31,24 @@ public class Controller implements EventListener {
 
     @Override
     public void move(Direction direction) {
-
+        model.move(direction);
+        view.update();
     }
 
     @Override
     public void restart() {
-
+        model.restart();
+        view.update();
     }
 
     @Override
     public void startNextLevel() {
-
+        model.startNextLevel();
+        view.update();
     }
 
     @Override
     public void levelCompleted(int level) {
-
+        view.completed(level);
     }
 }
