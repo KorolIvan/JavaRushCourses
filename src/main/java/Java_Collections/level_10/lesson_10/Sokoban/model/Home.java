1ddb83dbfb98c79b7a89ef7dc8e7848acf9a1f19
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-17 13:17:20 +0300

    Sokoban com.javarush.task.task34.task3410 done (6)

---
+++ src/main/java/Java_Collections/level_10/lesson_10/Sokoban/model/Home.java
@@ -0,0 +1,23 @@
+package Java_Collections.level_10.lesson_10.Sokoban.model;
+
+import java.awt.*;
+
+/**
+ * @author Ivan Korol on 7/17/2018
+ */
+public class Home extends GameObject {
+    public Home(int x, int y) {
+        super(x, y);
+        setWidth(2);
+        setHeight(2);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.RED);
+        int leftUpperCornerX = getX() - getWidth() / 2;
+        int leftUpperCornerY = getY() - getHeight() / 2;
+        graphics.drawRect(leftUpperCornerX, leftUpperCornerY, getWidth(), getHeight());
+        graphics.fillRect(leftUpperCornerX, leftUpperCornerY, getWidth(), getHeight());
+    }
+}
