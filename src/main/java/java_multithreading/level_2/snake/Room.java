676cfdef94c9252e0a9db371c7c25564ece82f05
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-19 15:18:39 +0300

    snake com.javarush.task.task23.task2312 done (9)

--- src/main/java/java_multithreading/level_2/snake/Room.java
+++ src/main/java/java_multithreading/level_2/snake/Room.java
@@ -4,7 +4,50 @@ package java_multithreading.level_2.snake;
  * @author Ivan Korol on 7/19/2018
  */
 public class Room {
+    private int width;
+    private int height;
+    private Snake snake;
+    private Mouse mouse;
+
+    public Room(int width, int height, Snake snake) {
+        this.width = width;
+        this.height = height;
+        this.snake = snake;
+    }
+
     public static void main(String[] args) {
 
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
     }
 }
