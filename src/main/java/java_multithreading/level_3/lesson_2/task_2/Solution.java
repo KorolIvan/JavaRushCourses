cde97dc8b8788e2f79b0b079102be8abe173b60d
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-19 17:45:55 +0300

    com.javarush.task.task23.task2302 done

---
+++ src/main/java/java_multithreading/level_3/lesson_2/task_2/Solution.java
@@ -0,0 +1,20 @@
+package java_multithreading.level_3.lesson_2.task_2;
+
+/**
+ * @author Ivan Korol on 7/19/2018
+ */
+public class Solution {
+    public static class Listener {
+        public final void onMouseDown(int x, int y) {
+            //do something on mouse down event
+        }
+
+        public void onMouseUp(int x, int y) {
+            //do something on mouse up event
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
