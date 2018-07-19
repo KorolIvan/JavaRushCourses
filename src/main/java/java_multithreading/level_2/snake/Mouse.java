bc981e16a89286ef1aac34a181103cb65f9d63ed
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-19 14:43:49 +0300

    snake com.javarush.task.task23.task2312 done (4)

--- src/main/java/java_multithreading/level_2/snake/Mouse.java
+++ src/main/java/java_multithreading/level_2/snake/Mouse.java
@@ -6,4 +6,17 @@ package java_multithreading.level_2.snake;
 public class Mouse {
     private int x;
     private int y;
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
