7642137fb8a6f5653cf72dea43c6f77452fd1107
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-19 14:49:53 +0300

    snake com.javarush.task.task23.task2312 done (7)

--- src/main/java/java_multithreading/level_2/snake/Snake.java
+++ src/main/java/java_multithreading/level_2/snake/Snake.java
@@ -1,7 +1,28 @@
 package java_multithreading.level_2.snake;
 
+import java.util.List;
+
 /**
  * @author Ivan Korol on 7/19/2018
  */
 public class Snake {
+    private List<SnakeSection> sections;
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
 }
