d9dc9cb830e92924976e456ecac29bf4c7e89482
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-23 17:24:13 +0300

    com.javarush.task.task23.task2310 done

---
+++ src/main/java/java_multithreading/level_3/lesson_8/task_1/Solution.java
@@ -0,0 +1,28 @@
+package java_multithreading.level_3.lesson_8.task_1;
+
+/**
+ * @author Ivan Korol on 7/23/2018
+ */
+public class Solution {
+    private String name;
+
+    Solution(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void sout() {
+        new Solution("sout") {
+            void printName() {
+                System.out.println(getName());
+            }
+        }.printName();
+    }
+
+    public static void main(String[] args) {
+        new Solution("main").sout();
+    }
+}
