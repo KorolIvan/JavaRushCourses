2690f2b21025078bd72ffbfa5c50698405f478c0
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-23 18:08:05 +0300

    Arkanoid com.javarush.task.task24.task2413 done (4)

--- src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Arkanoid.java
+++ src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Arkanoid.java
@@ -1,11 +1,16 @@
 package java_multithreading.level_3.lesson_13.Arkanoid;
 
+import java.util.List;
+
 /**
  * @author Ivan Korol on 7/23/2018
  */
 public class Arkanoid {
     private int width;
     private int height;
+    private Stand stand;
+    private Ball ball;
+    private List<Brick> bricks;
 
     public static void main(String[] args) {
 
@@ -30,5 +35,29 @@ public class Arkanoid {
 
     public void setHeight(int height) {
         this.height = height;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
     }
 }
