301b5016a330c62d03c6f694b59d888ae489847d
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-23 18:04:30 +0300

    Arkanoid com.javarush.task.task24.task2413 done (2)

--- src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Arkanoid.java
+++ src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Arkanoid.java
@@ -4,7 +4,25 @@ package java_multithreading.level_3.lesson_13.Arkanoid;
  * @author Ivan Korol on 7/23/2018
  */
 public class Arkanoid {
+    private int width;
+    private int height;
     public static void main(String[] args) {
 
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
