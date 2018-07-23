31b62a78a6221a07cbbe877dc8c3b7991cbd2dab
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-23 18:16:27 +0300

    Arkanoid com.javarush.task.task24.task2413 done (7)

--- src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/BaseObject.java
+++ src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/BaseObject.java
@@ -4,4 +4,37 @@ package java_multithreading.level_3.lesson_13.Arkanoid;
  * @author Ivan Korol on 7/23/2018
  */
 public class BaseObject {
+    private double x;
+    private double y;
+    private double radius;
+
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
--- src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Stand.java
+++ src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Stand.java
@@ -4,4 +4,7 @@ package java_multithreading.level_3.lesson_13.Arkanoid;
  * @author Ivan Korol on 7/23/2018
  */
 public class Stand extends BaseObject {
+    public Stand(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
