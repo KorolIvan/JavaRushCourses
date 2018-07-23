ca25cc71ead7c3c5a9a4c748e4aa66bb2a88419e
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-23 17:27:44 +0300

    com.javarush.task.task23.task2311 done

---
+++ src/main/java/java_multithreading/level_3/lesson_8/task_2/Solution.java
@@ -0,0 +1,52 @@
+package java_multithreading.level_3.lesson_8.task_2;
+
+/**
+ * @author Ivan Korol on 7/23/2018
+ */
+public class Solution {
+    public final String name;
+    public final String food;
+    public final String sound;
+
+    public Solution(String name, String food, String sound) {
+        this.name = name;
+        this.food = food;
+        this.sound = sound;
+    }
+
+    public void eat() {
+        System.out.println(name + ": Mmmmm, " + food);
+    }
+
+    public void play() {
+        System.out.println(name + ": " + sound + " " + sound);
+    }
+
+    public void sleep(long milliseconds) {
+        System.out.println(name + ": Zzzzzzz..." + (milliseconds / 1000) + " sec");
+    }
+
+    public void live() throws InterruptedException {
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    someActions();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            private void someActions() throws InterruptedException {
+                eat();
+                play();
+                Solution.this.sleep(1000);
+            }
+        };
+        thread.start();
+        thread.join();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Solution("Amigo", "beef", "knock").live();
+    }
+}
