c2f4ed3317bf22c23882234b87db97e8e76170f2
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-23 18:13:29 +0300

    Arkanoid com.javarush.task.task24.task2413 done (6)

--- src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Brick.java
+++ src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Brick.java
@@ -3,5 +3,5 @@ package java_multithreading.level_3.lesson_13.Arkanoid;
 /**
  * @author Ivan Korol on 7/23/2018
  */
-public class Brick {
+public class Brick extends BaseObject {
 }
--- src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Stand.java
+++ src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Stand.java
@@ -3,5 +3,5 @@ package java_multithreading.level_3.lesson_13.Arkanoid;
 /**
  * @author Ivan Korol on 7/23/2018
  */
-public class Stand {
+public class Stand extends BaseObject {
 }
