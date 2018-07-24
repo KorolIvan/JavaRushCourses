851475168b782b36dee67b608dfb3a51dab421d3
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-24 12:33:32 +0300

    Arkanoid com.javarush.task.task24.task2413 done (12)

--- src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Brick.java
+++ src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Brick.java
@@ -4,8 +4,8 @@ package java_multithreading.level_3.lesson_13.Arkanoid;
  * @author Ivan Korol on 7/23/2018
  */
 public class Brick extends BaseObject {
-    public Brick(double x, double y, double radius) {
-        super(x, y, radius);
+    public Brick(double x, double y) {
+        super(x, y, 3);
     }
 
     @Override
