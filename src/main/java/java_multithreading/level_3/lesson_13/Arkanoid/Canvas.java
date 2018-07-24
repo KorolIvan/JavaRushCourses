e7d4108de8bdd3468d88e71da3bf140bc24fc57e
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-24 12:15:50 +0300

    Arkanoid com.javarush.task.task24.task2413 done (10)

--- src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Canvas.java
+++ src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Canvas.java
@@ -14,6 +14,25 @@ public class Canvas {
         matrix = new char[height + 2][width + 2];
     }
 
+    public void setPoint(double x, double y, char c){
+        if(y >= 0 && y <= matrix.length && x >= 0 && x <= matrix.length) {
+            matrix[(int) Math.round(y)][(int) Math.round(x)] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        int height = matrix.length;
+        int width = matrix[0].length;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] == 1)
+                    setPoint(x + j, y + i, c);
+            }
+        }
+
+    }
+
     public int getWidth() {
         return width;
     }
