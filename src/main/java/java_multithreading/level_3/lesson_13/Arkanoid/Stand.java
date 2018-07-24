07e5de2ee00f8e2ac2c48161eff7fbe17da6aab1
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-24 12:59:16 +0300

    Arkanoid com.javarush.task.task24.task2413 done (16)

--- src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Stand.java
+++ src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Stand.java
@@ -4,8 +4,12 @@ package java_multithreading.level_3.lesson_13.Arkanoid;
  * @author Ivan Korol on 7/23/2018
  */
 public class Stand extends BaseObject {
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed;
+    private double direction;
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
     }
 
     @Override
@@ -16,5 +20,13 @@ public class Stand extends BaseObject {
     @Override
     public void move() {
 
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 }
