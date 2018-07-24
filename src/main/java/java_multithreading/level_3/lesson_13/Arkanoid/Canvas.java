7b5489ef966802e95ae00fcfd10da4f6175b2dd7
Ivan Korol <korol.vanja2008@gmail.com>
2018-07-24 11:57:54 +0300

    Arkanoid com.javarush.task.task24.task2413 done (9)

--- src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Canvas.java
+++ src/main/java/java_multithreading/level_3/lesson_13/Arkanoid/Canvas.java
@@ -4,4 +4,37 @@ package java_multithreading.level_3.lesson_13.Arkanoid;
  * @author Ivan Korol on 7/23/2018
  */
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height + 2][width + 2];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
 }
