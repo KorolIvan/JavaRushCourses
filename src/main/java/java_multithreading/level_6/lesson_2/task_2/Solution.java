dce5568a9b781eb825db0a5ccf9660f581540623
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-02 12:44:39 +0300

    com.javarush.task.task26.task2602 done

---
+++ src/main/java/java_multithreading/level_6/lesson_2/task_2/Solution.java
@@ -0,0 +1,35 @@
+package java_multithreading.level_6.lesson_2.task_2;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author Ivan Korol on 8/2/2018
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Set<Soldier> soldiers = new TreeSet<>();
+        soldiers.add(new Soldier("Ivanov", 170));
+        soldiers.add(new Soldier("Petrov", 180));
+        soldiers.add(new Soldier("Sidorov", 175));
+
+        for (Soldier soldier : soldiers) {
+            System.out.println(soldier.name);
+        }
+    }
+
+    public static class Soldier implements Comparable<Soldier> {
+        private String name;
+        private int height;
+
+        public Soldier(String name, int height) {
+            this.name = name;
+            this.height = height;
+        }
+
+        @Override
+        public int compareTo(Soldier o) {
+            return o.height - this.height;
+        }
+    }
+}
