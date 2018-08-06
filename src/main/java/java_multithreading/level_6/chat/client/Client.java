e205cae51f05674b0717654c710c137f7f4ae260
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-06 15:56:49 +0300

    chat com.javarush.task.task30.task3008 done (12)

---
+++ src/main/java/java_multithreading/level_6/chat/client/Client.java
@@ -0,0 +1,15 @@
+package java_multithreading.level_6.chat.client;
+
+import java_multithreading.level_6.chat.Connection;
+
+/**
+ * @author Ivan Korol on 8/6/2018
+ */
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread {
+
+    }
+}
