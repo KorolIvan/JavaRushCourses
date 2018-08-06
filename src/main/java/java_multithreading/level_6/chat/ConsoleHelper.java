7dbc97e445e6537b9d8a17a430c27c41658c5c78
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-06 10:14:03 +0300

    chat com.javarush.task.task30.task3008 done (2)

--- src/main/java/java_multithreading/level_6/chat/ConsoleHelper.java
+++ src/main/java/java_multithreading/level_6/chat/ConsoleHelper.java
@@ -1,7 +1,37 @@
 package java_multithreading.level_6.chat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author Ivan Korol on 8/6/2018
  */
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        while (true) {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(readString());
+            }catch (NumberFormatException ex) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+
+    }
 }
