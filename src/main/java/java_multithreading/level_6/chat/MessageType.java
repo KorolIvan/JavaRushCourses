0105ea5af38150d80479fa5e37c86831e796ff56
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-06 10:26:03 +0300

    chat com.javarush.task.task30.task3008 done (3)

--- src/main/java/java_multithreading/level_6/chat/MessageType.java
+++ src/main/java/java_multithreading/level_6/chat/MessageType.java
@@ -4,4 +4,10 @@ package java_multithreading.level_6.chat;
  * @author Ivan Korol on 8/6/2018
  */
 public enum MessageType {
+    NAME_REQUEST,
+    USER_NAME,
+    NAME_ACCEPTED,
+    TEXT,
+    USER_ADDED,
+    USER_REMOVED
 }
