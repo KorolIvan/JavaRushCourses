a56b91bb7df69e6b801475caadc82524d455f275
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-06 12:04:12 +0300

    chat com.javarush.task.task30.task3008 done (7)

--- src/main/java/java_multithreading/level_6/chat/Server.java
+++ src/main/java/java_multithreading/level_6/chat/Server.java
@@ -3,11 +3,14 @@ package java_multithreading.level_6.chat;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Ivan Korol on 8/6/2018
  */
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
     public static void main(String[] args) {
         int port = ConsoleHelper.readInt();
         Socket socket;
@@ -21,6 +24,15 @@ public class Server {
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message){
+        try{
+            for (Map.Entry<String, Connection> pair : connectionMap.entrySet())
+                pair.getValue().send(message);
+        }catch(IOException e){
+            System.out.println("Сообщение не отправлено");
         }
     }
 
