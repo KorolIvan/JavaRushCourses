87c6a612b53c8ff926f14bd4ab648e6913cda8f0
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-06 10:02:29 +0300

    chat com.javarush.task.task30.task3008 done (1)

---
+++ src/main/java/java_multithreading/level_6/chat/MessageType.java
@@ -0,0 +1,7 @@
+package java_multithreading.level_6.chat;
+
+/**
+ * @author Ivan Korol on 8/6/2018
+ */
+public enum MessageType {
+}
---
+++ src/main/java/java_multithreading/level_6/chat/Server.java
@@ -0,0 +1,10 @@
+package java_multithreading.level_6.chat;
+
+/**
+ * @author Ivan Korol on 8/6/2018
+ */
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
