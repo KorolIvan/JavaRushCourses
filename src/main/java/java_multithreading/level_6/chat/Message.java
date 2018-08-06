643bc602991e16bf8834585d5a2bb90768145390
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-06 10:38:40 +0300

    chat com.javarush.task.task30.task3008 done (4)

--- src/main/java/java_multithreading/level_6/chat/Message.java
+++ src/main/java/java_multithreading/level_6/chat/Message.java
@@ -1,7 +1,29 @@
 package java_multithreading.level_6.chat;
 
+import java.io.Serializable;
+
 /**
  * @author Ivan Korol on 8/6/2018
  */
-public class Message {
+public class Message implements Serializable {
+    private final MessageType type;
+    private final String data;
+
+    public Message(MessageType type) {
+        this.type = type;
+        this.data = null;
+    }
+
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
 }
