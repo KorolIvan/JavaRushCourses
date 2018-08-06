be8b5795fecc56b4317f42a641b6d7dc594a033d
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-06 11:51:00 +0300

    chat com.javarush.task.task30.task3008 done (6)

--- src/main/java/java_multithreading/level_6/chat/Server.java
+++ src/main/java/java_multithreading/level_6/chat/Server.java
@@ -1,10 +1,35 @@
 package java_multithreading.level_6.chat;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author Ivan Korol on 8/6/2018
  */
 public class Server {
     public static void main(String[] args) {
+        int port = ConsoleHelper.readInt();
+        Socket socket;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен");
+            while (true) {
+                socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        private Handler(Socket socket) {
+            this.socket = socket;
+        }
 
     }
 }
