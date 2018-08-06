393e2f5e9754aa8cbcbbd8b14f4da8590affbb2a
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-06 17:46:07 +0300

    chat com.javarush.task.task30.task3008 done (18)

---
+++ src/main/java/java_multithreading/level_6/chat/client/BotClient.java
@@ -0,0 +1,32 @@
+package java_multithreading.level_6.chat.client;
+
+/**
+ * @author Ivan Korol on 8/6/2018
+ */
+public class BotClient extends Client {
+
+    public static void main(String[] args) {
+        BotClient botClient = new BotClient();
+        botClient.run();
+    }
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new BotSocketThread();
+    }
+
+    @Override
+    protected boolean shouldSendTextFromConsole() {
+        return false;
+    }
+
+    @Override
+    protected String getUserName() {
+        return "date_bot_" + (int) (Math.random()*100);
+    }
+
+    public class BotSocketThread extends SocketThread{
+
+
+    }
+}
