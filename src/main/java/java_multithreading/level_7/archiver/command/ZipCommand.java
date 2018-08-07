6a40d43b5b5c6cff19baba6f29627a6c7f0359a1
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-07 15:04:38 +0300

    archiver com.javarush.task.task31.task3110 done (5)

---
+++ src/main/java/java_multithreading/level_7/archiver/command/ZipCommand.java
@@ -0,0 +1,7 @@
+package java_multithreading.level_7.archiver.command;
+
+/**
+ * @author Ivan Korol on 8/7/2018
+ */
+public abstract class ZipCommand implements Command {
+}
---
+++ src/main/java/java_multithreading/level_7/archiver/command/ZipExtractCommand.java
@@ -0,0 +1,11 @@
+package java_multithreading.level_7.archiver.command;
+
+/**
+ * @author Ivan Korol on 8/7/2018
+ */
+public class ZipExtractCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
