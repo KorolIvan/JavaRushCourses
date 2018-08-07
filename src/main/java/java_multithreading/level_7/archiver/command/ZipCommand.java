dad262a35ed08536022d290ddc0f3cd35ad4b402
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-07 15:47:27 +0300

    archiver com.javarush.task.task31.task3110 done (11)

--- src/main/java/java_multithreading/level_7/archiver/command/ZipCommand.java
+++ src/main/java/java_multithreading/level_7/archiver/command/ZipCommand.java
@@ -1,7 +1,18 @@
 package java_multithreading.level_7.archiver.command;
 
+import java_multithreading.level_7.archiver.ConsoleHelper;
+import java_multithreading.level_7.archiver.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author Ivan Korol on 8/7/2018
  */
 public abstract class ZipCommand implements Command {
+    public ZipFileManager getZipFileManager() throws Exception{
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
+    }
 }
--- src/main/java/java_multithreading/level_7/archiver/command/ZipCreateCommand.java
+++ src/main/java/java_multithreading/level_7/archiver/command/ZipCreateCommand.java
@@ -1,11 +1,27 @@
 package java_multithreading.level_7.archiver.command;
 
+import java_multithreading.level_7.archiver.ConsoleHelper;
+import java_multithreading.level_7.archiver.ZipFileManager;
+import java_multithreading.level_7.archiver.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author Ivan Korol on 8/7/2018
  */
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
+            ZipFileManager zipFileManager = getZipFileManager();
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(sourcePath);
+            ConsoleHelper.writeMessage("Архив создан.");
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
