98c9facb01278154882241dd1e9ea780fc9e933e
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-07 15:55:39 +0300

    archiver com.javarush.task.task31.task3110 done (14)

--- src/main/java/java_multithreading/level_7/archiver/command/ZipContentCommand.java
+++ src/main/java/java_multithreading/level_7/archiver/command/ZipContentCommand.java
@@ -1,11 +1,24 @@
 package java_multithreading.level_7.archiver.command;
 
+import java_multithreading.level_7.archiver.ConsoleHelper;
+import java_multithreading.level_7.archiver.FileProperties;
+import java_multithreading.level_7.archiver.ZipFileManager;
+
+import java.util.List;
+
 /**
  * @author Ivan Korol on 8/7/2018
  */
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+        ZipFileManager zipFileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Содержимое архива:");
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
+        }
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
