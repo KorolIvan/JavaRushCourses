60996d68b893274f136952a7188f180b4696d55a
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-07 11:46:44 +0300

    com.javarush.task.task27.task2702 done

---
+++ src/main/java/java_multithreading/level_7/lesson_2/task_2/Solution.java
@@ -0,0 +1,19 @@
+package java_multithreading.level_7.lesson_2.task_2;
+
+/**
+ * @author Ivan Korol on 8/7/2018
+ */
+public class Solution {
+    public static void main(String args[]) {
+        label:
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j > i) {
+                    System.out.println();
+                    continue label;
+                }
+                System.out.print("S");
+            }
+        }
+    }
+}
