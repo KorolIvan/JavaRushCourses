4d6a5b0da89293688394b340d164f47b27797c63
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-07 16:00:08 +0300

    archiver com.javarush.task.task31.task3110 done (15)

--- src/main/java/java_multithreading/level_7/archiver/command/ZipExtractCommand.java
+++ src/main/java/java_multithreading/level_7/archiver/command/ZipExtractCommand.java
@@ -1,11 +1,27 @@
 package java_multithreading.level_7.archiver.command;
 
+import java_multithreading.level_7.archiver.ConsoleHelper;
+import java_multithreading.level_7.archiver.ZipFileManager;
+import java_multithreading.level_7.archiver.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author Ivan Korol on 8/7/2018
  */
 public class ZipExtractCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        try {
+            ConsoleHelper.writeMessage("Распаковка архива.");
+            ZipFileManager zipFileManager = getZipFileManager();
+            ConsoleHelper.writeMessage("Введите путь для распаковки:");
+            Path outputPath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(outputPath);
+            ConsoleHelper.writeMessage("Архив распакован.");
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
