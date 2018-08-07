775700f045dc2f523a9e5b1ca99e37305acef7bd
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-07 16:11:04 +0300

    archiver com.javarush.task.task31.task3110 done (17)

--- src/main/java/java_multithreading/level_7/archiver/command/ZipAddCommand.java
+++ src/main/java/java_multithreading/level_7/archiver/command/ZipAddCommand.java
@@ -1,11 +1,27 @@
 package java_multithreading.level_7.archiver.command;
 
+import java_multithreading.level_7.archiver.ConsoleHelper;
+import java_multithreading.level_7.archiver.ZipFileManager;
+import java_multithreading.level_7.archiver.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author Ivan Korol on 8/7/2018
  */
 public class ZipAddCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        try {
+            ConsoleHelper.writeMessage("Добавление нового файла в архив.");
+            ZipFileManager zipFileManager = getZipFileManager();
+            ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.addFile(sourcePath);
+            ConsoleHelper.writeMessage("Добавление в архив завершено.");
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Файл не был найден.");
+        }
     }
 }
