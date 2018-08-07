1554db2d3532a401ae1b38d8771fe327d083ddba
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-07 12:00:04 +0300

    com.javarush.task.task27.task2703 done

---
+++ src/main/java/java_multithreading/level_7/lesson_4/task_1/Solution.java
@@ -0,0 +1,48 @@
+package java_multithreading.level_7.lesson_4.task_1;
+
+/**
+ * @author Ivan Korol on 8/7/2018
+ */
+public class Solution {
+    public static class Friend {
+        private final String name;
+
+        public Friend(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public synchronized void bow(Friend bower) {
+            System.out.format("%s: %s"
+                            + " has bowed to me!%n",
+                    this.name, bower.getName());
+            bower.bowBack(this);
+        }
+
+        public synchronized void bowBack(Friend bower) {
+            System.out.format("%s: %s"
+                            + " has bowed back to me!%n",
+                    this.name, bower.getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        final Friend alphonse =
+                new Friend("Alphonse");
+        final Friend gaston =
+                new Friend("Gaston");
+        new Thread(new Runnable() {
+            public void run() {
+                alphonse.bow(gaston);
+            }
+        }).start();
+        new Thread(new Runnable() {
+            public void run() {
+                gaston.bow(alphonse);
+            }
+        }).start();
+    }
+}
