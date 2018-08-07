6013fd496a8c8c504042924be52e0a65bc2ce6cc
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-07 14:28:22 +0300

    archiver com.javarush.task.task31.task3110 done (1)

---
+++ src/main/java/java_multithreading/level_7/archiver/Archiver.java
@@ -0,0 +1,20 @@
+package java_multithreading.level_7.archiver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+/**
+ * @author Ivan Korol on 8/7/2018
+ */
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter full path to the archive: ");
+        String pathToArchive = reader.readLine();
+        ZipFileManager zipFileManager  = new ZipFileManager(Paths.get(pathToArchive));
+        System.out.println("enter full path to the file which should be archived");
+        String pathToFile = reader.readLine();
+        zipFileManager.createZip(Paths.get(pathToFile));
+    }
+}
---
+++ src/main/java/java_multithreading/level_7/archiver/ZipFileManager.java
@@ -0,0 +1,18 @@
+package java_multithreading.level_7.archiver;
+
+import java.nio.file.Path;
+
+/**
+ * @author Ivan Korol on 8/7/2018
+ */
+public class ZipFileManager {
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile) {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception {
+
+    }
+}
