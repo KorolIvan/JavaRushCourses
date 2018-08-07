1c359242a4348aa762ab58660bdbbf29f3686161
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-07 14:31:25 +0300

    archiver com.javarush.task.task31.task3110 done (2)

--- src/main/java/java_multithreading/level_7/archiver/ZipFileManager.java
+++ src/main/java/java_multithreading/level_7/archiver/ZipFileManager.java
@@ -1,6 +1,10 @@
 package java_multithreading.level_7.archiver;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @author Ivan Korol on 8/7/2018
@@ -13,6 +17,17 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
-
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+             InputStream inputStream = Files.newInputStream(source)) {
+            ZipEntry sourceEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(sourceEntry);
+            //Считываем содержимое файла в массив byte
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            //Добавляем содержимое к архиву
+            zipOutputStream.write(buffer);
+            //Закрываем текущую запись для новой записи
+            zipOutputStream.closeEntry();
+        }
     }
 }
