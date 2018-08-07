0be21edff387fc992084a0166980b01a0dfc0295
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-07 12:06:23 +0300

    com.javarush.task.task27.task2704 done

---
+++ src/main/java/java_multithreading/level_7/lesson_4/task_3/Solution.java
@@ -0,0 +1,43 @@
+package java_multithreading.level_7.lesson_4.task_3;
+
+/**
+ * @author Ivan Korol on 8/7/2018
+ */
+public class Solution {
+    private final String field;
+
+    public Solution(String field) {
+        this.field = field;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public synchronized void sout(Solution solution) {
+        System.out.format("111:  %s: %s %n", this.field, solution.getField());
+        solution.sout2(this);
+    }
+
+    public synchronized void sout2(Solution solution) {
+        System.out.format("222:  %s: %s %n", this.field, solution.getField());
+        solution.sout(this);
+    }
+
+    public static void main(String[] args) {
+        final Solution solution = new Solution("first");
+        final Solution solution2 = new Solution("second");
+        new Thread(new Runnable() {
+            public void run() {
+                solution.sout(solution2);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                solution2.sout(solution);
+            }
+        }).start();
+
+    }
+}
