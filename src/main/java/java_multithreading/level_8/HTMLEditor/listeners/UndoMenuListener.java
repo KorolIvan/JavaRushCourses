0d6be7d34325b2535d70b0dcb4ab317fd9ff0646
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-17 13:29:44 +0300

    HTMLEditor com.javarush.task.task32.task3209(10) done

--- src/main/java/java_multithreading/level_8/HTMLEditor/listeners/UndoMenuListener.java
+++ src/main/java/java_multithreading/level_8/HTMLEditor/listeners/UndoMenuListener.java
@@ -22,7 +22,8 @@ public class UndoMenuListener implements MenuListener {
 
     @Override
     public void menuSelected(MenuEvent e) {
-
+        undoMenuItem.setEnabled(view.canUndo());
+        redoMenuItem.setEnabled(view.canRedo());
     }
 
     @Override
