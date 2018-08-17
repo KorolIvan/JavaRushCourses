fe8a137d64674558ba577d264ea2e255fb99e503
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-17 10:53:27 +0300

    HTMLEditor com.javarush.task.task32.task3209(6) done

--- src/main/java/java_multithreading/level_8/HTMLEditor/View.java
+++ src/main/java/java_multithreading/level_8/HTMLEditor/View.java
@@ -1,8 +1,10 @@
 package java_multithreading.level_8.HTMLEditor;
 
 import java_multithreading.level_8.HTMLEditor.listeners.FrameListener;
+import java_multithreading.level_8.HTMLEditor.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -32,7 +34,12 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
-
+       htmlTextPane.setContentType("text/html");
+       tabbedPane.addTab("HTML", new JScrollPane(htmlTextPane));
+       tabbedPane.addTab("Текст", new JScrollPane(plainTextPane));
+       tabbedPane.setPreferredSize(new Dimension(800, 600));
+       tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+       getContentPane().add(tabbedPane, BorderLayout.CENTER);
     }
 
     public void initGui() {
