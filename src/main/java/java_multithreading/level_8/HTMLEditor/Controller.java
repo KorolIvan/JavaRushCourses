1284338be34172bb9f81822c192c37a6f0f4d39e
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-21 12:37:24 +0300

    HTMLEditor com.javarush.task.task32.task3209(17) done

--- src/main/java/java_multithreading/level_8/HTMLEditor/Controller.java
+++ src/main/java/java_multithreading/level_8/HTMLEditor/Controller.java
@@ -6,6 +6,7 @@ import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 /**
  * @author Ivan Korol on 8/16/2018
@@ -40,6 +41,16 @@ public class Controller {
         } catch (IOException | BadLocationException e) {
             ExceptionHandler.log(e);
         }
+    }
+
+    public String getPlainText(){
+        StringWriter stringWriter = new StringWriter();
+        try {
+            new HTMLEditorKit().write(stringWriter, document, 0, document.getLength());
+        } catch (IOException | BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+        return stringWriter.toString();
     }
 
     public void exit() {
