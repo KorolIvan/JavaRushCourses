08ca731ee4fd75cac97e0114b5dd831dc9b25fb9
Ivan Korol <korol.vanja2008@gmail.com>
2018-08-31 10:15:29 +0300

    restaurant com.javarush.task.task27.task2712 done (13)

--- src/main/java/java_multithreading/level_9/lesson_9/restaurant/statistic/StatisticManager.java
+++ src/main/java/java_multithreading/level_9/lesson_9/restaurant/statistic/StatisticManager.java
@@ -1,12 +1,19 @@
 package java_multithreading.level_9.lesson_9.restaurant.statistic;
 
 import java_multithreading.level_9.lesson_9.restaurant.statistic.event.EventDataRow;
+import java_multithreading.level_9.lesson_9.restaurant.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Ivan Korol on 8/31/2018
  */
 public class StatisticManager {
     private static StatisticManager statisticManager = new StatisticManager();
+    private StatisticStorage statisticStorage = new StatisticStorage();
 
     public static StatisticManager getInstance() {
         return statisticManager;
@@ -16,6 +23,17 @@ public class StatisticManager {
     }
 
     public void register(EventDataRow data) {
+
+    }
+
+    private class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<EventType, List<EventDataRow>>();
+
+        private StatisticStorage() {
+            for(EventType type: EventType.values()) {
+                storage.put(type, new ArrayList<EventDataRow>());
+            }
+        }
 
     }
 }
