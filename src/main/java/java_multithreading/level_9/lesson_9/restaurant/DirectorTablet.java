dd57e7eb1aca5b52bd6ea47b10441048ca10a79d
Ivan Korol <korol.vanja2008@gmail.com>
2018-09-03 11:27:43 +0300

    restaurant com.javarush.task.task27.task2712 done (16)

--- src/main/java/java_multithreading/level_9/lesson_9/restaurant/DirectorTablet.java
+++ src/main/java/java_multithreading/level_9/lesson_9/restaurant/DirectorTablet.java
@@ -1,15 +1,38 @@
 package java_multithreading.level_9.lesson_9.restaurant;
 
+import java_multithreading.level_9.lesson_9.restaurant.statistic.StatisticManager;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
 /**
  * @author Ivan Korol on 9/3/2018
  */
 public class DirectorTablet {
-    public void printAdvertisementProfit() {
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
 
+    public void printAdvertisementProfit() {
+        double total = 0;
+        StatisticManager statisticManager = StatisticManager.getInstance();
+        for(Map.Entry<Date, Double> pair: statisticManager.getTotalAmountForAdvertisement().entrySet()) {
+            double profit = (double) pair.getValue();
+            ConsoleHelper.writeMessage(String.format("%s - %.2f", simpleDateFormat.format(pair.getKey()), profit));
+            total += profit;
+        }
+        ConsoleHelper.writeMessage(String.format("Total - %.2f", total));
     }
 
     public void printCookWorkloading() {
-
+        StatisticManager statisticManager = StatisticManager.getInstance();
+        for (Map.Entry<Date, Map<String, Integer>> pair : statisticManager.getCookeerInfo().entrySet()) {
+            ConsoleHelper.writeMessage(simpleDateFormat.format(pair.getKey()));
+            for (Map.Entry<String, Integer> pair2 : pair.getValue().entrySet()) {
+                ConsoleHelper.writeMessage(String.format("%s - %d min", pair2.getKey(), pair2.getValue()));
+            }
+            ConsoleHelper.writeMessage("");
+        }
     }
 
     public void printActiveVideoSet() {
--- src/main/java/java_multithreading/level_9/lesson_9/restaurant/statistic/StatisticManager.java
+++ src/main/java/java_multithreading/level_9/lesson_9/restaurant/statistic/StatisticManager.java
@@ -1,8 +1,10 @@
 package java_multithreading.level_9.lesson_9.restaurant.statistic;
 
 import java_multithreading.level_9.lesson_9.restaurant.kitchen.Cook;
+import java_multithreading.level_9.lesson_9.restaurant.statistic.event.CookedOrderEventDataRow;
 import java_multithreading.level_9.lesson_9.restaurant.statistic.event.EventDataRow;
 import java_multithreading.level_9.lesson_9.restaurant.statistic.event.EventType;
+import java_multithreading.level_9.lesson_9.restaurant.statistic.event.VideoSelectedEventDataRow;
 
 import java.util.*;
 
@@ -40,9 +42,69 @@ public class StatisticManager {
             storage.put(data.getType(), eventDataRows);
         }
 
+        private List<EventDataRow> get(EventType eventType) {
+            return storage.get(eventType);
+        }
+
     }
 
     public void register(Cook cook) {
         cooks.add(cook);
+    }
+
+    public Map<Date, Double> getTotalAmountForAdvertisement() {
+        Map<Date, Double> result = new TreeMap<Date, Double>(Collections.reverseOrder());
+        List<EventDataRow> eventDataRowList = statisticStorage.get(EventType.SELECTED_VIDEOS);
+        Double amount;
+        Date date;
+        Calendar calendar;
+        for (EventDataRow eventDataRow : eventDataRowList) {
+            VideoSelectedEventDataRow adVideo = (VideoSelectedEventDataRow) eventDataRow;
+            amount = adVideo.getAmount() * 0.01d;
+            calendar = Calendar.getInstance();
+            calendar.setTime(adVideo.getDate());
+            GregorianCalendar gc = new GregorianCalendar(
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+            );
+            date = gc.getTime();
+            if (result.containsKey(date))
+                amount += result.get(date);
+            result.put(date, amount);
+        }
+        return result;
+    }
+
+    public Map<Date, Map<String, Integer>> getCookeerInfo() {
+        Map<Date, Map<String, Integer>> result = new TreeMap<Date, Map<String, Integer>>(Collections.reverseOrder());
+        List<EventDataRow> eventDataRowList = statisticStorage.get(EventType.COOKED_ORDER);
+        Date date;
+        Calendar calendar;
+        String cookName;
+        int cookingTimeSeconds;
+        for (EventDataRow eventDataRow : eventDataRowList) {
+            CookedOrderEventDataRow cookedOrder = (CookedOrderEventDataRow) eventDataRow;
+            calendar = Calendar.getInstance();
+            calendar.setTime(cookedOrder.getDate());
+            GregorianCalendar gc = new GregorianCalendar(
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+            );
+            date = gc.getTime();
+            cookName = cookedOrder.getCookName();
+            cookingTimeSeconds = cookedOrder.getTime();
+            Map<String, Integer> cookTime = new TreeMap<String, Integer>();
+            if (result.containsKey(date)) {
+                cookTime = result.get(date);
+                if (cookTime.containsKey(cookName)) {
+                    cookingTimeSeconds += cookTime.get(cookName);
+                }
+            }
+            cookTime.put(cookName, cookingTimeSeconds);
+            result.put(date, cookTime);
+        }
+        return result;
     }
 }
