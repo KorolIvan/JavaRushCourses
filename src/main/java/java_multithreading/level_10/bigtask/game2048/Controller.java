a736832f0103b9fc1d9bb7153393a748b7dfef5c
Ivan Korol <korol.vanja2008@gmail.com>
2018-09-11 12:24:02 +0300

    2048 com.javarush.task.task35.task3513 (1) done

---
+++ src/main/java/java_multithreading/level_10/bigtask/game2048/Controller.java
@@ -0,0 +1,7 @@
+package java_multithreading.level_10.bigtask.game2048;
+
+/**
+ * @author Ivan Korol on 9/11/2018
+ */
+public class Controller {
+}
---
+++ src/main/java/java_multithreading/level_10/bigtask/game2048/Main.java
@@ -0,0 +1,10 @@
+package java_multithreading.level_10.bigtask.game2048;
+
+/**
+ * @author Ivan Korol on 9/11/2018
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
