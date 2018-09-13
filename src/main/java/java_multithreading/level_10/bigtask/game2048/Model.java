4142dc077455eb5dae860273a21141c727bcc3da
Ivan Korol <korol.vanja2008@gmail.com>
2018-09-13 12:00:51 +0300

    2048 com.javarush.task.task35.task3513 (3) done

--- src/main/java/java_multithreading/level_10/bigtask/game2048/Model.java
+++ src/main/java/java_multithreading/level_10/bigtask/game2048/Model.java
@@ -4,4 +4,14 @@ package java_multithreading.level_10.bigtask.game2048;
  * @author Ivan Korol on 9/11/2018
  */
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+    public Model() {
+        for(int i = 0; i < FIELD_WIDTH; i++) {
+            for(int j = 0; j < FIELD_WIDTH; j++){
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
